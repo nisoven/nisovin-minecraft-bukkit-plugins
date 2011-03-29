8772cba0e3b22d78cbd83d13054e448e1b8bd02f
nisovin@gmail.com <nisovin@gmail.com@3b5476c5-0415-9417-c352-9b123fd710cf>
2011-03-29 05:59:59 +0000

    Started adding some functions

--- trunk/SkillSystem/src/com/nisovin/SkillSystem/SkillSystem.java
+++ trunk/SkillSystem/src/com/nisovin/SkillSystem/SkillSystem.java
@@ -1,17 +1,28 @@
 package com.nisovin.SkillSystem;
 
+import org.bukkit.Server;
+import org.bukkit.event.Event;
+import org.bukkit.event.Event.Priority;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SkillSystem extends JavaPlugin {
 
-	@Override
-	public void onDisable() {
-		// TODO Auto-generated method stub
-		
-	}
+	public static Server server;
 
 	@Override
 	public void onEnable() {
+		
+		server = getServer();
+		
+		SSBlockListener blockListener = new SSBlockListener(this);
+		
+		server.getPluginManager().registerEvent(Event.Type.BLOCK_BREAK, blockListener, Priority.Normal, this);
+		server.getPluginManager().registerEvent(Event.Type.BLOCK_PLACED, blockListener, Priority.Normal, this);
+		
+	}
+	
+	@Override
+	public void onDisable() {
 		// TODO Auto-generated method stub
 		
 	}
