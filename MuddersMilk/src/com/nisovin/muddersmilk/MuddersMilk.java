2ded75aec13d584fe6d6ca09c576095d1a960cd8
nisovin <nisovin@gmail.com>
2011-05-13 05:21:29 +0000

    Version update

--- MuddersMilk/src/com/nisovin/muddersmilk/MuddersMilk.java
+++ MuddersMilk/src/com/nisovin/muddersmilk/MuddersMilk.java
@@ -1,5 +1,8 @@
 package com.nisovin.muddersmilk;
 
+//import java.io.File;
+//import java.io.FileOutputStream;
+//import java.io.InputStream;
 import java.util.HashMap;
 
 import org.bukkit.entity.Player;
@@ -55,6 +58,34 @@ public class MuddersMilk extends JavaPlugin {
 		effects = null;
 	}
 
+	/*public void loadConfigFromJar() {
+		File configFile = new File(this.getDataFolder(), "config.yml");
+        if (!configFile.exists()) {
+            InputStream fis = getClass().getResourceAsStream("/config.yml");
+            FileOutputStream fos = null;
+            try {
+            	fos = new FileOutputStream(configFile);
+                byte[] buf = new byte[1024];
+                int i = 0;
+                while ((i = fis.read(buf)) != -1) {
+                    fos.write(buf, 0, i);
+                }
+            } catch (Exception e) {
+            	getServer().getLogger().severe("Failed to load config from JAR");
+            } finally {
+            	try {
+	                if (fis != null) {
+	                    fis.close();
+	                }
+	                if (fos != null) {
+	                    fos.close();
+	                }
+            	} catch (Exception e) {            		
+            	}
+            }
+        }
+	}*/
+	
 	@Override
 	public void onDisable() {
 		if (effects != null) {
