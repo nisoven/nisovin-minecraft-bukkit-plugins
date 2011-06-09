8f55b5e3ff26254c9a15fe681f1271635acefe5a
nisovin@gmail.com <nisovin@gmail.com@3b5476c5-0415-9417-c352-9b123fd710cf>
2011-06-09 18:59:42 +0000

    Added option to turn of explosion from fireball

--- trunk/MagicSpells/src/com/nisovin/MagicSpells/MagicEntityListener.java
+++ trunk/MagicSpells/src/com/nisovin/MagicSpells/MagicEntityListener.java
@@ -6,12 +6,14 @@ import org.bukkit.event.Event;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityListener;
 import org.bukkit.event.entity.EntityTargetEvent;
+import org.bukkit.event.entity.ExplosionPrimeEvent;
 
 public class MagicEntityListener extends EntityListener {
 	
 	public MagicEntityListener(MagicSpells plugin) {
 		plugin.getServer().getPluginManager().registerEvent(Event.Type.ENTITY_DAMAGE, this, Event.Priority.Normal, plugin);
 		plugin.getServer().getPluginManager().registerEvent(Event.Type.ENTITY_TARGET, this, Event.Priority.Normal, plugin);
+		plugin.getServer().getPluginManager().registerEvent(Event.Type.EXPLOSION_PRIME, this, Event.Priority.Normal, plugin);
 	}
 	
 	@Override
@@ -30,6 +32,16 @@ public class MagicEntityListener extends EntityListener {
 		if (spells != null) {
 			for (Spell spell : spells) {
 				spell.onEntityTarget(event);
+			}
+		}		
+	}
+	
+	@Override
+	public void onExplosionPrime(ExplosionPrimeEvent event) {
+		HashSet<Spell> spells = MagicSpells.listeners.get(Event.Type.EXPLOSION_PRIME);
+		if (spells != null) {
+			for (Spell spell : spells) {
+				spell.onExplosionPrime(event);
 			}
 		}		
 	}
