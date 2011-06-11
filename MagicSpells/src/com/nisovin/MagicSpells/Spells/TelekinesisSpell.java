5f165297e44d80f0c5b4000d9f26a5e918d4f0f8
nisovin <nisovin@gmail.com>
2011-06-11 02:29:49 +0000

    Fixed telekinesis

--- MagicSpells/src/com/nisovin/MagicSpells/Spells/TelekinesisSpell.java
+++ MagicSpells/src/com/nisovin/MagicSpells/Spells/TelekinesisSpell.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.CraftWorld;
 import org.bukkit.entity.Player;
 import org.bukkit.util.config.Configuration;
 
@@ -36,6 +37,9 @@ public class TelekinesisSpell extends InstantSpell {
 		transparent = new HashSet<Byte>();
 		transparent.add((byte)Material.AIR.getId());
 		transparent.add((byte)Material.REDSTONE_WIRE.getId());
+		transparent.add((byte)Material.REDSTONE_TORCH_ON.getId());
+		transparent.add((byte)Material.REDSTONE_TORCH_OFF.getId());
+		transparent.add((byte)Material.TORCH.getId());
 	}
 	
 	protected boolean castSpell(Player player, SpellCastState state, String[] args) {
@@ -46,9 +50,13 @@ public class TelekinesisSpell extends InstantSpell {
 				sendMessage(player, strNoTarget);
 				return true;
 			} else if (target.getType() == Material.LEVER || target.getType() == Material.STONE_BUTTON) {
-				target.setData((byte) (target.getData() ^ 0x8));
+				//target.setData((byte) (target.getData() ^ 0x8));
+				net.minecraft.server.Block.byId[target.getType().getId()].interact(((CraftWorld)target.getWorld()).getHandle(), target.getX(), target.getY(), target.getZ(), null);
 			} else if (target.getType() == Material.WOOD_PLATE || target.getType() == Material.STONE_PLATE) {
-				target.setData((byte) (target.getData() ^ 0x1));				
+				target.setData((byte) (target.getData() ^ 0x1));
+				net.minecraft.server.World w = ((CraftWorld)target.getWorld()).getHandle();
+				w.applyPhysics(target.getX(), target.getY(), target.getZ(), target.getType().getId());
+				w.applyPhysics(target.getX(), target.getY()-1, target.getZ(), target.getType().getId());
 			} else {
 				// fail
 				sendMessage(player, strNoTarget);
