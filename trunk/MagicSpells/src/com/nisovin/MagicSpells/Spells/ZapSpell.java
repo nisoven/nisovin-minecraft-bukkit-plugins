9d02199a27dadfb693485465fa71bc25e27fc0e7
nisovin@gmail.com <nisovin@gmail.com@3b5476c5-0415-9417-c352-9b123fd710cf>
2011-06-19 23:07:31 +0000

    Added animation to zap spell

--- trunk/MagicSpells/src/com/nisovin/MagicSpells/Spells/ZapSpell.java
+++ trunk/MagicSpells/src/com/nisovin/MagicSpells/Spells/ZapSpell.java
@@ -1,5 +1,8 @@
 package com.nisovin.MagicSpells.Spells;
 
+import java.util.HashSet;
+
+import org.bukkit.Effect;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -15,6 +18,7 @@ public class ZapSpell extends InstantSpell {
 	private static final String SPELL_NAME = "zap";
 	
 	private String strCantZap;
+	private HashSet<Byte> transparentBlockTypes;
 	private int[] disallowedBlockTypes;
 	private boolean dropBlock;
 	
@@ -32,8 +36,12 @@ public class ZapSpell extends InstantSpell {
 		super(config, spellName);
 		
 		strCantZap = config.getString("spells." + spellName + ".str-cant-zap", "");
-		
-		String[] disallowed = config.getString("spells." + spellName + ".disallowed-block-types","0,7").split(",");
+		String[] transparent = config.getString("spells." + spellName + ".transparent-block-types","0,8,9").split(",");
+		String[] disallowed = config.getString("spells." + spellName + ".disallowed-block-types","0,7,10,11").split(",");
+		transparentBlockTypes = new HashSet<Byte>();
+		for (String s : transparent) {
+			transparentBlockTypes.add(Byte.parseByte(s));
+		}
 		disallowedBlockTypes = new int [disallowed.length];
 		for (int i = 0; i < disallowed.length; i++) {
 			disallowedBlockTypes[i] = Integer.parseInt(disallowed[i]);
@@ -45,7 +53,7 @@ public class ZapSpell extends InstantSpell {
 	protected boolean castSpell(Player player, SpellCastState state, String[] args) {
 		if (state == SpellCastState.NORMAL) {
 			// get targeted block
-			Block target = player.getTargetBlock(null, range>0?range:100);
+			Block target = player.getTargetBlock(transparentBlockTypes, range>0?range:100);
 			if (target != null) {
 				// check for disallowed block
 				for (int i = 0; i < disallowedBlockTypes.length; i++) {
@@ -66,6 +74,8 @@ public class ZapSpell extends InstantSpell {
 					if (dropBlock) {
 						target.getWorld().dropItemNaturally(target.getLocation(), new ItemStack(target.getType(), 1, target.getData()));
 					}
+					// show animation
+					player.getWorld().playEffect(target.getLocation(), Effect.STEP_SOUND, target.getTypeId());
 					// remove block
 					target.setType(Material.AIR);
 				}
