bfe9e37c0c3fe06a549b4e3fc10cf2a1fa4f9611
nisovin <nisovin@gmail.com>
2011-07-12 17:33:48 +0000

    No-magic zone stuff that got missed

---
+++ MagicSpells/src/com/nisovin/MagicSpells/Util/NoMagicZone.java
@@ -0,0 +1,68 @@
+package com.nisovin.MagicSpells.Util;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
+
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+
+public class NoMagicZone {
+
+	private ProtectedRegion region = null;
+	private Vector point1 = null;
+	private Vector point2 = null;
+	
+	public NoMagicZone(ProtectedRegion region) {
+		this.region = region;
+	}
+	
+	public NoMagicZone(Vector v1, Vector v2) {
+		int minx, miny, minz, maxx, maxy, maxz;
+		if (v1.getX() < v2.getX()) {
+			minx = v1.getBlockX();
+			maxx = v2.getBlockX();
+		} else {
+			minx = v2.getBlockX();
+			maxx = v1.getBlockX();
+		}
+		if (v1.getY() < v2.getY()) {
+			miny = v1.getBlockY();
+			maxy = v2.getBlockY();
+		} else {
+			miny = v2.getBlockY();
+			maxy = v1.getBlockY();
+		}
+		if (v1.getZ() < v2.getZ()) {
+			minz = v1.getBlockZ();
+			maxz = v2.getBlockZ();
+		} else {
+			minz = v2.getBlockZ();
+			maxz = v1.getBlockZ();
+		}
+		point1 = new Vector(minx, miny, minz);
+		point2 = new Vector(maxx, maxy, maxz);
+	}
+	
+	public boolean inZone(Player player) {
+		return inZone(player.getLocation());
+	}
+	
+	public boolean inZone(Location location) {
+		if (region != null) {
+			com.sk89q.worldedit.Vector v = new com.sk89q.worldedit.Vector(location.getX(), location.getY(), location.getZ());
+			return region.contains(v);
+		} else {
+			int x = location.getBlockX();
+			int y = location.getBlockY();
+			int z = location.getBlockZ();
+			if (point1.getBlockX() <= x && x <= point2.getBlockX() &&
+					point1.getBlockY() <= y && y <= point2.getBlockY() &&
+					point1.getBlockZ() <= z && z <= point2.getBlockZ()) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+	
+}
---
+++ MagicSpells/src/com/nisovin/MagicSpells/Util/NoMagicZoneManager.java
@@ -0,0 +1,62 @@
+package com.nisovin.MagicSpells.Util;
+
+import java.util.HashSet;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
+import org.bukkit.util.config.Configuration;
+
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+
+public class NoMagicZoneManager {
+	
+	private HashSet<NoMagicZone> zones;
+
+	public NoMagicZoneManager(Configuration config) {
+		zones = new HashSet<NoMagicZone>();
+		
+		WorldGuardPlugin worldGuard = null;
+		if (Bukkit.getServer().getPluginManager().isPluginEnabled("WorldGuard")) {
+			worldGuard = (WorldGuardPlugin)Bukkit.getServer().getPluginManager().getPlugin("WorldGuard");
+		}
+		
+		List<String> zoneList = config.getStringList("no-magic-zones", null);
+		if (zoneList != null) {
+			for (String s : zoneList) {
+				String[] data = s.split(":");
+				if (data[0].equalsIgnoreCase("worldguard") && worldGuard != null) {
+					World w = Bukkit.getServer().getWorld(data[1]);
+					zones.add(new NoMagicZone(worldGuard.getRegionManager(w).getRegion(data[2])));
+				} else if (data[0].equalsIgnoreCase("cuboid")) {
+					String[] p1 = data[1].split(",");
+					String[] p2 = data[2].split(",");
+					Vector point1 = new Vector(Integer.parseInt(p1[0]), Integer.parseInt(p1[1]), Integer.parseInt(p1[2]));
+					Vector point2 = new Vector(Integer.parseInt(p2[0]), Integer.parseInt(p2[1]), Integer.parseInt(p2[2]));
+					zones.add(new NoMagicZone(point1, point2));
+				}
+			}
+		}
+	}
+	
+	public boolean inNoMagicZone(Player player) {
+		return inNoMagicZone(player.getLocation());
+	}
+	
+	public boolean inNoMagicZone(Location location) {
+		for (NoMagicZone zone : zones) {
+			if (zone.inZone(location)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public int zoneCount() {
+		return zones.size();
+	}
+	
+}
