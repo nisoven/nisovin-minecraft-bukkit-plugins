8835618b98d32a321395308831858505b69ad42d
nisovin <nisovin@gmail.com>
2011-09-02 18:48:21 +0000

    MS: external spell now has temp perms, also now uses lists for commands in config

--- MagicSpells/src/com/nisovin/magicspells/spells/ExternalCommandSpell.java
+++ MagicSpells/src/com/nisovin/magicspells/spells/ExternalCommandSpell.java
@@ -1,5 +1,7 @@
 package com.nisovin.magicspells.spells;
 
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
@@ -16,10 +18,11 @@ public class ExternalCommandSpell extends InstantSpell {
 
 	private boolean castWithItem;
 	private boolean castByCommand;
-	private String[] commandToExecute;
-	private String[] commandToExecuteLater;
+	private List<String> commandToExecute;
+	private List<String> commandToExecuteLater;
 	private int commandDelay;
-	private String[] commandToBlock;
+	private List<String> commandToBlock;
+	private List<String> temporaryPermissions;
 	private boolean requirePlayerTarget;
 	private boolean obeyLos;
 	private String strCantUseCommand;
@@ -32,10 +35,11 @@ public class ExternalCommandSpell extends InstantSpell {
 		
 		castWithItem = config.getBoolean("spells." + spellName + ".can-cast-with-item", true);
 		castByCommand = config.getBoolean("spells." + spellName + ".can-cast-by-command", true);
-		commandToExecute = config.getString("spells." + spellName + ".command-to-execute", "").split("\\|\\|");
-		commandToExecuteLater = config.getString("spells." + spellName + ".command-to-execute-later", "").split("\\|\\|");
+		commandToExecute = getConfigStringList("command-to-execute", null);
+		commandToExecuteLater = getConfigStringList("command-to-execute-later", null);
 		commandDelay = getConfigInt("command-delay", 0);
-		commandToBlock = config.getString("spells." + spellName + ".command-to-block", "").split("\\|\\|");
+		commandToBlock = getConfigStringList("command-to-block", null);
+		temporaryPermissions = getConfigStringList("temporary-permissions", null);
 		requirePlayerTarget = getConfigBoolean("require-player-target", false);
 		obeyLos = getConfigBoolean("obey-los", true);
 		strCantUseCommand = config.getString("spells." + spellName + ".str-cant-use-command", "&4You don't have permission to do that.");
@@ -48,6 +52,7 @@ public class ExternalCommandSpell extends InstantSpell {
 			Bukkit.getServer().getLogger().severe("MagicSpells: External command spell '" + name + "' has no command to execute.");
 			return PostCastAction.ALREADY_HANDLED;
 		} else if (state == SpellCastState.NORMAL) {
+			// get target if necessary
 			Player target = null;
 			if (requirePlayerTarget) {
 				target = getTargetedPlayer(player, range, obeyLos);
@@ -56,6 +61,15 @@ public class ExternalCommandSpell extends InstantSpell {
 					return PostCastAction.ALREADY_HANDLED;
 				}
 			}
+			// grant permissions
+			if (temporaryPermissions != null) {
+				for (String perm : temporaryPermissions) {
+					if (!player.hasPermission(perm)) {
+						player.addAttachment(MagicSpells.plugin, perm, true, 5);
+					}
+				}
+			}
+			// perform commands
 			for (String comm : commandToExecute) {
 				if (args != null && args.length > 0) {
 					for (int i = 0; i < args.length; i++) {
@@ -68,8 +82,8 @@ public class ExternalCommandSpell extends InstantSpell {
 				}
 				player.performCommand(comm);
 			}
-			if (commandToExecuteLater != null && commandToExecuteLater.length > 0 && !commandToExecuteLater[0].isEmpty()) {
-				Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(MagicSpells.plugin, new DelayedCommand(player), commandDelay);
+			if (commandToExecuteLater != null && commandToExecuteLater.size() > 0 && !commandToExecuteLater.get(0).isEmpty()) {
+				Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(MagicSpells.plugin, new DelayedCommand(player, target), commandDelay);
 			}
 		}
 		return PostCastAction.HANDLE_NORMALLY;
@@ -77,7 +91,7 @@ public class ExternalCommandSpell extends InstantSpell {
 	
 	@Override
 	public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
-		if (!event.getPlayer().isOp() && commandToBlock.length > 0) {
+		if (!event.getPlayer().isOp() && commandToBlock != null && commandToBlock.size() > 0) {
 			String msg = event.getMessage();
 			for (String comm : commandToBlock) {
 				comm = comm.trim();
@@ -103,15 +117,21 @@ public class ExternalCommandSpell extends InstantSpell {
 	private class DelayedCommand implements Runnable {
 
 		private Player player;
+		private Player target;
 		
-		public DelayedCommand(Player player) {
+		public DelayedCommand(Player player, Player target) {
 			this.player = player;
+			this.target = target;
 		}
 		
 		@Override
 		public void run() {
 			for (String comm : commandToExecuteLater) {
 				if (comm != null && !comm.isEmpty()) {
+					comm = comm.replace("%a", player.getName());
+					if (target != null) {
+						comm = comm.replace("%t", target.getName());
+					}
 					player.performCommand(comm);
 				}
 			}			
