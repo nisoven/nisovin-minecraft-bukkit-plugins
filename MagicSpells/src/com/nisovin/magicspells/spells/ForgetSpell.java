a6c366c5895d397f35056cbab8add12b657a1feb
nisovin <nisovin@gmail.com>
2011-10-04 00:51:38 +0000

    MS: Reworked forget spell to allow self-forgetting

--- MagicSpells/src/com/nisovin/magicspells/spells/ForgetSpell.java
+++ MagicSpells/src/com/nisovin/magicspells/spells/ForgetSpell.java
@@ -13,82 +13,112 @@ import com.nisovin.magicspells.Spellbook;
 
 public class ForgetSpell extends CommandSpell {
 
+	private boolean allowSelfForget;
 	private String strUsage;
 	private String strNoTarget;
 	private String strNoSpell;
 	private String strDoesntKnow;
 	private String strCastTarget;
+	private String strCastSelfTarget;
+	private String strResetTarget;
+	private String strResetSelf;
 	
 	public ForgetSpell(Configuration config, String spellName) {
 		super(config, spellName);
 		
+		allowSelfForget = getConfigBoolean("allow-self-forget", true);
 		strUsage = config.getString("spells." + spellName + ".str-usage", "Usage: /cast forget <target> <spell>");
 		strNoTarget = config.getString("spells." + spellName + ".str-no-target", "No such player.");
 		strNoSpell = config.getString("spells." + spellName + ".str-no-spell", "You do not know a spell by that name.");
 		strDoesntKnow = config.getString("spells." + spellName + ".str-doesnt-know", "That person does not know that spell.");
 		strCastTarget = config.getString("spells." + spellName + ".str-cast-target", "%a has made you forget the %s spell.");
+		strCastSelfTarget = getConfigString("str-cast-self-target", "You have forgotten the %s spell.");
+		strResetTarget = getConfigString("str-reset-target", "You have reset %t's spellbook.");
+		strResetSelf = getConfigString("str-reset-self", "You have forgotten all of your spells.");
 	}
 	
 	@Override
 	protected PostCastAction castSpell(Player player, SpellCastState state, String[] args) {
 		if (state == SpellCastState.NORMAL) {
-			if (args == null || args.length != 2) {
+			if (args == null || args.length == 0 || args.length > 2) {
 				// fail: missing args
 				sendMessage(player, strUsage);
-			} else {
+				return PostCastAction.ALREADY_HANDLED;
+			}
+				// get caster spellbook
+			Spellbook casterSpellbook = MagicSpells.getSpellbook(player);
+			
+			// get target
+			Player target = null;
+			if (args.length == 1 && allowSelfForget) {
+				target = player;
+			} else if (args.length == 2 && casterSpellbook.hasAdvancedPerm()) {
 				List<Player> players = MagicSpells.plugin.getServer().matchPlayer(args[0]);
 				if (players.size() != 1) {
 					// fail: no player match
 					sendMessage(player, strNoTarget);
+					return PostCastAction.ALREADY_HANDLED;				
 				} else {
-					boolean all = false;
-					Spell spell = null;
-					if (args[1].equals("*")) {
-						all = true;
-					} else {
-						spell = MagicSpells.getSpellByInGameName(args[1]);
-					}
-					if (spell == null && !all) {
-						// fail: no spell match
-						sendMessage(player, strNoSpell);
-					} else {
-						Spellbook spellbook = MagicSpells.getSpellbook(player);
-						if (spellbook == null) {
-							// fail: missing spellbook
-							sendMessage(player, strNoSpell);							
-						} else if (!all && !spellbook.hasSpell(spell)) {
-							// fail: player doesn't have spell
-							sendMessage(player, strNoSpell);
-						} else if (all && !spellbook.hasAdvancedPerm()) {
-							// fail: caster wants to remove all spells but doesn't have advanced perms
-							sendMessage(player, strNoSpell);
-						} else {
-							// yay! can forget!
-							Spellbook targetSpellbook = MagicSpells.getSpellbook(players.get(0));
-							if (targetSpellbook == null) {
-								// fail: missing spellbook
-								sendMessage(player, strDoesntKnow);
-							} else if (!all && !targetSpellbook.hasSpell(spell)) {
-								// fail: no spellbook for some reason or can't learn the spell
-								sendMessage(player, strDoesntKnow);
-							} else if (!all) {
-								targetSpellbook.removeSpell(spell);
-								targetSpellbook.save();
-								sendMessage(players.get(0), formatMessage(strCastTarget, "%a", player.getDisplayName(), "%s", spell.getName(), "%t", players.get(0).getDisplayName()));
-								sendMessage(player, formatMessage(strCastSelf, "%a", player.getDisplayName(), "%s", spell.getName(), "%t", players.get(0).getDisplayName()));
-								return PostCastAction.NO_MESSAGES;
-							} else if (all) {
-								targetSpellbook.removeAllSpells();
-								targetSpellbook.addGrantedSpells();
-								targetSpellbook.save();
-								sendMessage(player, "You have reset " + players.get(0).getName() + "'s spellbook.");
-								return PostCastAction.NO_MESSAGES;
-							}
-						}
-					}
+					target = players.get(0);
 				}
+			} else {
+				// fail: missing args
+				sendMessage(player, strUsage);
+				return PostCastAction.ALREADY_HANDLED;
 			}
-			return PostCastAction.ALREADY_HANDLED;
+			
+			// get spell
+			String spellName = args.length == 1 ? args[0] : args[1];
+			boolean all = false;
+			Spell spell = null;
+			if (spellName.equals("*")) {
+				all = true;
+			} else {
+				spell = MagicSpells.getSpellByInGameName(spellName);
+			}
+			if (spell == null && !all) {
+				// fail: no spell match
+				sendMessage(player, strNoSpell);
+				return PostCastAction.ALREADY_HANDLED;
+			}
+			
+			// check if caster has spell
+			if (!all && !casterSpellbook.hasSpell(spell)) {
+				// fail: caster doesn't have spell
+				sendMessage(player, strNoSpell);
+				return PostCastAction.ALREADY_HANDLED;
+			}
+			
+			// get target spellbook and perform checks
+			Spellbook targetSpellbook = MagicSpells.getSpellbook(target);
+			if (targetSpellbook == null || (!all && !targetSpellbook.hasSpell(spell))) {
+				// fail: error
+				sendMessage(player, strDoesntKnow);
+				return PostCastAction.ALREADY_HANDLED;
+			} 
+			
+			// remove spell(s)
+			if (!all) {
+				targetSpellbook.removeSpell(spell);
+				targetSpellbook.save();
+				if (!player.equals(target)) {
+					sendMessage(target, formatMessage(strCastTarget, "%a", player.getDisplayName(), "%s", spell.getName(), "%t", target.getDisplayName()));
+					sendMessage(player, formatMessage(strCastSelf, "%a", player.getDisplayName(), "%s", spell.getName(), "%t", target.getDisplayName()));
+				} else {
+					sendMessage(player, strCastSelfTarget, "%s", spell.getName());
+				}
+				return PostCastAction.NO_MESSAGES;
+			} else if (all) {
+				targetSpellbook.removeAllSpells();
+				targetSpellbook.addGrantedSpells();
+				targetSpellbook.save();
+				if (!player.equals(target)) {
+					sendMessage(player, strResetTarget, "%t", target.getDisplayName());
+				} else {
+					sendMessage(player, strResetSelf);
+				}
+				return PostCastAction.NO_MESSAGES;
+			}
 		}
 		return PostCastAction.HANDLE_NORMALLY;
 	}
