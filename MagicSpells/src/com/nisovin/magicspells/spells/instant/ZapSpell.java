d3f329f170f3ceadbc6e3ecd6319f520123a5c17
nisovin <nisovin@gmail.com>
2011-10-17 23:20:11 +0000

    MS: Added new options for zap: allowed-block-types, check-plugins, drop-normal

--- MagicSpells/src/com/nisovin/magicspells/spells/instant/ZapSpell.java
+++ MagicSpells/src/com/nisovin/magicspells/spells/instant/ZapSpell.java
@@ -1,10 +1,13 @@
 package com.nisovin.magicspells.spells.instant;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 import org.bukkit.Effect;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.CraftWorld;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
@@ -17,24 +20,34 @@ public class ZapSpell extends InstantSpell {
 	
 	private String strCantZap;
 	private HashSet<Byte> transparentBlockTypes;
-	private int[] disallowedBlockTypes;
+	private List<Integer> allowedBlockTypes;
+	private List<Integer> disallowedBlockTypes;
 	private boolean dropBlock;
+	private boolean dropNormal;
+	private boolean checkPlugins;
 	
 	public ZapSpell(MagicConfig config, String spellName) {
 		super(config, spellName);
 		
-		strCantZap = config.getString("spells." + spellName + ".str-cant-zap", "");
-		String[] transparent = config.getString("spells." + spellName + ".transparent-block-types","0,8,9").split(",");
-		String[] disallowed = config.getString("spells." + spellName + ".disallowed-block-types","0,7,10,11").split(",");
+		strCantZap = getConfigString("str-cant-zap", "");
+		String[] transparent = getConfigString("transparent-block-types","0,8,9").split(",");
+		String[] allowed = getConfigString("allowed-block-types","").split(",");
+		String[] disallowed = getConfigString("disallowed-block-types","0,7,10,11").split(",");
 		transparentBlockTypes = new HashSet<Byte>();
 		for (String s : transparent) {
 			transparentBlockTypes.add(Byte.parseByte(s));
 		}
-		disallowedBlockTypes = new int [disallowed.length];
-		for (int i = 0; i < disallowed.length; i++) {
-			disallowedBlockTypes[i] = Integer.parseInt(disallowed[i]);
+		allowedBlockTypes = new ArrayList<Integer>();
+		for (String s : allowed) {
+			allowedBlockTypes.add(Integer.parseInt(s));
+		}
+		disallowedBlockTypes = new ArrayList<Integer>();
+		for (String s : disallowed) {
+			disallowedBlockTypes.add(Integer.parseInt(s));
 		}
 		dropBlock = config.getBoolean("spells." + spellName + ".drop-block", false);
+		dropNormal = getConfigBoolean("drop-normal", true);
+		checkPlugins = getConfigBoolean("check-plugins", true);
 	}
 
 	@Override
@@ -44,29 +57,37 @@ public class ZapSpell extends InstantSpell {
 			Block target = player.getTargetBlock(transparentBlockTypes, range>0?range:100);
 			if (target != null) {
 				// check for disallowed block
-				for (int i = 0; i < disallowedBlockTypes.length; i++) {
-					if (target.getTypeId() == disallowedBlockTypes[i]) {
+				if (disallowedBlockTypes.contains(target.getTypeId()) || (allowedBlockTypes.size() > 0 && !allowedBlockTypes.contains(target.getTypeId()))) {
+					sendMessage(player, strCantZap);
+					return PostCastAction.ALREADY_HANDLED;
+				}
+				
+				// check for protection
+				if (checkPlugins) {
+					BlockBreakEvent event = new BlockBreakEvent(target, player);
+					MagicSpells.plugin.getServer().getPluginManager().callEvent(event);
+					if (event.isCancelled()) {
+						// a plugin cancelled the event
 						sendMessage(player, strCantZap);
 						return PostCastAction.ALREADY_HANDLED;
 					}
 				}
-				// check for protection
-				BlockBreakEvent event = new BlockBreakEvent(target, player);
-				MagicSpells.plugin.getServer().getPluginManager().callEvent(event);
-				if (event.isCancelled()) {
-					// a plugin cancelled the event
-					sendMessage(player, strCantZap);
-					return PostCastAction.ALREADY_HANDLED;
-				} else {
-					// drop block
-					if (dropBlock) {
+				
+				// drop block
+				if (dropBlock) {
+					if (dropNormal) {
+						net.minecraft.server.Block.byId[target.getTypeId()].dropNaturally(((CraftWorld)target.getWorld()).getHandle(), target.getX(), target.getY(), target.getZ(), target.getData(), 1.0F);
+					} else {
 						target.getWorld().dropItemNaturally(target.getLocation(), new ItemStack(target.getType(), 1, target.getData()));
 					}
-					// show animation
-					player.getWorld().playEffect(target.getLocation(), Effect.STEP_SOUND, target.getTypeId());
-					// remove block
-					target.setType(Material.AIR);
 				}
+				
+				// show animation
+				player.getWorld().playEffect(target.getLocation(), Effect.STEP_SOUND, target.getTypeId());
+				
+				// remove block
+				target.setType(Material.AIR);
+				
 			} else {
 				sendMessage(player, strCantZap);
 				return PostCastAction.ALREADY_HANDLED;
