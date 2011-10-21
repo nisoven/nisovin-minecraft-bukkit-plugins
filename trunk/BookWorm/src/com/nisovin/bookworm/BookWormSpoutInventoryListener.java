7cbc0ec2497159fb8f55c6db95f58820d622f868
nisovin@gmail.com <nisovin@gmail.com@3b5476c5-0415-9417-c352-9b123fd710cf>
2011-10-21 23:11:11 +0000

    BW: Removed book click title achievement feature

--- trunk/BookWorm/src/com/nisovin/bookworm/BookWormSpoutInventoryListener.java
+++ trunk/BookWorm/src/com/nisovin/bookworm/BookWormSpoutInventoryListener.java
@@ -5,7 +5,6 @@ import org.bukkit.event.Event;
 import org.bukkit.inventory.ItemStack;
 import org.getspout.spoutapi.event.inventory.InventoryClickEvent;
 import org.getspout.spoutapi.event.inventory.InventoryListener;
-import org.getspout.spoutapi.player.SpoutPlayer;
 
 public class BookWormSpoutInventoryListener extends InventoryListener {
 
@@ -18,18 +17,6 @@ public class BookWormSpoutInventoryListener extends InventoryListener {
 		ItemStack itemClicked = event.getItem();
 		ItemStack itemHolding = event.getCursor();
 		if (itemClicked != null && itemClicked.getType() == Material.BOOK) {
-			if (BookWorm.BOOK_INFO_ACHIEVEMENT && itemHolding == null && !event.isLeftClick() && itemClicked.getDurability() != 0) {
-				Book book = BookWorm.getBook(itemClicked);
-				if (book != null) {
-					SpoutPlayer player = (SpoutPlayer)event.getPlayer();
-					String title = book.getTitle();
-					if (title.length() > 26) title = title.substring(0, 26);
-					String byLine = BookWorm.S_READ_BY + " " + book.getAuthor();
-					if (byLine.length() > 26) byLine = byLine.substring(0, 26);
-					player.sendNotification(title, byLine, Material.BOOK);
-					event.setCancelled(true);
-				}
-			}
 			if (itemHolding != null && itemHolding.getType() == Material.BOOK && itemClicked.getDurability() != itemHolding.getDurability()) {
 				event.setCancelled(true);
 			}
