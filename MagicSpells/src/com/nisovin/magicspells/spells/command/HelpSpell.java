12a8ab7afaeaa818a0f1e76d8e3e70331a59b08e
nisovin <nisovin@gmail.com>
2012-01-16 15:51:49 +0000

    MS: added require-known-spell option to help spell

--- MagicSpells/src/com/nisovin/magicspells/spells/command/HelpSpell.java
+++ MagicSpells/src/com/nisovin/magicspells/spells/command/HelpSpell.java
@@ -11,6 +11,7 @@ import com.nisovin.magicspells.util.MagicConfig;
 
 public class HelpSpell extends CommandSpell {
 	
+	private boolean requireKnownSpell;
 	private String strUsage;
 	private String strNoSpell;
 	private String strDescLine;
@@ -19,6 +20,7 @@ public class HelpSpell extends CommandSpell {
 	public HelpSpell(MagicConfig config, String spellName) {
 		super(config, spellName);
 		
+		requireKnownSpell = getConfigBoolean("require-known-spell", true);
 		strUsage = config.getString("spells." + spellName + ".str-usage", "Usage: /cast " + name + " <spell>");
 		strNoSpell = config.getString("spells." + spellName + ".str-no-spell", "You do not know a spell by that name.");
 		strDescLine = config.getString("spells." + spellName + ".str-desc-line", "%s - %d");
@@ -34,7 +36,7 @@ public class HelpSpell extends CommandSpell {
 			} else {
 				Spell spell = MagicSpells.getSpellByInGameName(args[0]);
 				Spellbook spellbook = MagicSpells.getSpellbook(player);
-				if (spell == null || spellbook == null || !spellbook.hasSpell(spell)) {
+				if (spell == null || (requireKnownSpell && (spellbook == null || !spellbook.hasSpell(spell)))) {
 					sendMessage(player, strNoSpell);
 					return PostCastAction.ALREADY_HANDLED;
 				} else {
