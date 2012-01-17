7aac2103e05089dee12e3613b97485d83aa134b8
nisovin <nisovin@gmail.com>
2012-01-17 21:57:47 +0000

    MS: added delay option for multispell

--- MagicSpells/src/com/nisovin/magicspells/MultiSpell.java
+++ MagicSpells/src/com/nisovin/magicspells/MultiSpell.java
@@ -14,7 +14,7 @@ public final class MultiSpell extends Spell {
 	private boolean castByCommand;
 	private boolean checkIndividualCooldowns;
 	
-	private ArrayList<Spell> spells;
+	private ArrayList<Action> actions;
 	
 	public MultiSpell(MagicConfig config, String spellName) {
 		super(config, spellName);
@@ -26,15 +26,20 @@ public final class MultiSpell extends Spell {
 		castByCommand = config.getBoolean("multispells." + spellName + ".can-cast-by-command", true);
 		checkIndividualCooldowns = config.getBoolean("multispells." + spellName + ".check-individual-cooldowns", false);
 
-		spells = new ArrayList<Spell>();
+		actions = new ArrayList<Action>();
 		List<String> spellList = config.getStringList("multispells." + spellName + ".spells", null);
 		if (spellList != null) {
 			for (String s : spellList) {
-				Spell spell = MagicSpells.getSpellByInternalName(s);
-				if (spell != null) {
-					spells.add(spell);
+				if (s.matches("DELAY [0-9]+")) {
+					int delay = Integer.parseInt(s.split(" ")[1]);
+					actions.add(new Action(delay));
 				} else {
-					Bukkit.getServer().getLogger().severe("MagicSpells: no such spell '" + s + "' for multi-spell '" + spellName + "'");
+					Spell spell = MagicSpells.getSpellByInternalName(s);
+					if (spell != null) {
+						actions.add(new Action(spell));
+					} else {
+						Bukkit.getServer().getLogger().severe("MagicSpells: no such spell '" + s + "' for multi-spell '" + spellName + "'");
+					}
 				}
 			}
 		}
@@ -45,17 +50,30 @@ public final class MultiSpell extends Spell {
 		if (state == SpellCastState.NORMAL) {
 			// check cooldowns
 			if (checkIndividualCooldowns) {
-				for (Spell spell : spells) {
-					if (spell.onCooldown(player)) {
-						// a spell is on cooldown
-						sendMessage(player, MagicSpells.strOnCooldown);
-						return PostCastAction.ALREADY_HANDLED;
+				for (Action action : actions) {
+					if (action.isSpell()) {
+						if (action.getSpell().onCooldown(player)) {
+							// a spell is on cooldown
+							sendMessage(player, MagicSpells.strOnCooldown);
+							return PostCastAction.ALREADY_HANDLED;
+						}
 					}
 				}
 			}
 			
-			for (Spell spell : spells) {
-				spell.castSpell(player, SpellCastState.NORMAL, power, null);
+			int delay = 0;
+			Spell spell;
+			for (Action action : actions) {
+				if (action.isDelay()) {
+					delay += action.getDelay();
+				} else if (action.isSpell()) {
+					spell = action.getSpell();
+					if (delay == 0) {
+						spell.castSpell(player, SpellCastState.NORMAL, power, null);
+					} else {
+						Bukkit.getScheduler().scheduleSyncDelayedTask(MagicSpells.plugin, new DelayedSpell(spell, player, power), delay);
+					}
+				}
 			}
 		}
 		return PostCastAction.HANDLE_NORMALLY;
@@ -69,6 +87,54 @@ public final class MultiSpell extends Spell {
 	@Override
 	public boolean canCastByCommand() {
 		return castByCommand;
+	}
+	
+	private class Action {
+		private Spell spell;
+		private int delay;
+		
+		public Action(Spell spell) {
+			this.spell = spell;
+			this.delay = 0;
+		}
+		
+		public Action(int delay) {
+			this.delay = delay;
+			this.spell = null;
+		}
+		
+		public boolean isSpell() {
+			return spell != null;
+		}
+		
+		public Spell getSpell() {
+			return spell;
+		}
+		
+		public boolean isDelay() {
+			return delay > 0;
+		}
+		
+		public int getDelay() {
+			return delay;
+		}
+	}
+	
+	private class DelayedSpell implements Runnable {
+		private Spell spell;
+		private Player player;
+		private float power;
+		
+		public DelayedSpell(Spell spell, Player player, float power) {
+			this.spell = spell;
+			this.player = player;
+			this.power = power;
+		}
+		
+		@Override
+		public void run() {
+			spell.castSpell(player, SpellCastState.NORMAL, power, null);
+		}
 	}
 
 }
