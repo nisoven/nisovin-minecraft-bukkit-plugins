d178c32e79bd6cc78d4855b7613c922dbe85fe71
nisovin <nisovin@gmail.com>
2012-01-27 23:26:06 +0000

    MS: Added bind restrictions

--- MagicSpells/src/com/nisovin/magicspells/spells/command/BindSpell.java
+++ MagicSpells/src/com/nisovin/magicspells/spells/command/BindSpell.java
@@ -1,5 +1,8 @@
 package com.nisovin.magicspells.spells.command;
 
+import java.util.HashSet;
+import java.util.List;
+
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -12,16 +15,26 @@ import com.nisovin.magicspells.util.MagicConfig;
 
 public class BindSpell extends CommandSpell {
 	
+	private HashSet<CastItem> bindableItems;
 	private String strUsage;
 	private String strNoSpell;
-	private String strCantBind;
+	private String strCantBindSpell;
+	private String strCantBindItem;
 
 	public BindSpell(MagicConfig config, String spellName) {
 		super(config, spellName);
 		
+		List<String> bindables = getConfigStringList("bindable-items", null);
+		if (bindables != null) {
+			bindableItems = new HashSet<CastItem>();
+			for (String s : bindables) {
+				bindableItems.add(new CastItem(s));
+			}
+		}
 		strUsage = config.getString("spells." + spellName + ".str-usage", "You must specify a spell name and hold an item in your hand.");
 		strNoSpell = config.getString("spells." + spellName + ".str-no-spell", "You do not know a spell by that name.");
-		strCantBind = config.getString("spells." + spellName + ".str-cant-bind", "That spell cannot be bound to an item.");
+		strCantBindSpell = config.getString("spells." + spellName + ".str-cant-bind-spell", "That spell cannot be bound to an item.");
+		strCantBindItem = config.getString("spells." + spellName + ".str-cant-bind-item", "That spell cannot be bound to that item.");
 	}
 
 	@Override
@@ -35,26 +48,34 @@ public class BindSpell extends CommandSpell {
 				if (spell == null || spellbook == null) {
 					// fail - no such spell, or no spellbook
 					sendMessage(player, strNoSpell);
+					return PostCastAction.ALREADY_HANDLED;
 				} else if (!spellbook.hasSpell(spell)) {
 					// fail - doesn't know spell
 					sendMessage(player, strNoSpell);
+					return PostCastAction.ALREADY_HANDLED;
 				} else if (!spell.canCastWithItem()) {
 					// fail - spell can't be bound
-					sendMessage(player, strCantBind);
+					sendMessage(player, strCantBindSpell);
+					return PostCastAction.ALREADY_HANDLED;
 				} else {
-					int typeId = player.getItemInHand()==null?-1:player.getItemInHand().getTypeId();
-					spellbook.removeSpell(spell);
-					if (typeId <= 0) {
-						spellbook.addSpell(spell, new CastItem(-1));
+					CastItem castItem = new CastItem(player.getItemInHand());
+					if (bindableItems != null && !bindableItems.contains(castItem)) {
+						sendMessage(player, strCantBindItem);
+						return PostCastAction.ALREADY_HANDLED;
+					} else if (!spell.canBind(castItem)) {
+						String msg = spell.getCantBindError();
+						if (msg == null) msg = strCantBindItem;
+						sendMessage(player, msg);
+						return PostCastAction.ALREADY_HANDLED;						
 					} else {
+						spellbook.removeSpell(spell);
 						spellbook.addSpell(spell, new CastItem(player.getItemInHand()));
+						spellbook.save();
+						sendMessage(player, formatMessage(strCastSelf, "%s", spell.getName()));
+						return PostCastAction.NO_MESSAGES;
 					}
-					spellbook.save();
-					sendMessage(player, formatMessage(strCastSelf, "%s", spell.getName()));
-					return PostCastAction.NO_MESSAGES;
 				}
 			}
-			return PostCastAction.ALREADY_HANDLED;
 		}		
 		return PostCastAction.HANDLE_NORMALLY;
 	}
