e69e4f41e6162b71a36a8c92ed31b9e9bb678b89
nisovin <nisovin@gmail.com>
2012-02-02 05:42:56 +0000

    WL: sign edits, door locks, new commands and sign code, hub world generator

--- WorldLoader/src/com/nisovin/worldloader/SignExecutor.java
+++ WorldLoader/src/com/nisovin/worldloader/SignExecutor.java
@@ -1,6 +1,7 @@
 package com.nisovin.worldloader;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -25,21 +26,29 @@ public class SignExecutor {
 		for (int i = 1; i < 4; i++) {
 			String line = lines[i];
 			if (line != null && !line.isEmpty()) {
+				if (i < 3 && lines[i+1] != null && lines[i+1].startsWith("... ")) {
+					line += " " + lines[i+1].substring(4);
+					i++;
+				}
 				executeLine(line, block, player);
 			}
 		}
 	}
 	
 	private static void executeLine(String line, Block block, Player player) {
-		if (line.matches("^SET -?[0-9]*,-?[0-9]*,-?[0-9]* [0-9]+(:[0-9]+)?$")) {
+		if (line.matches("^S(ET|T)? -?[0-9]*,-?[0-9]*,-?[0-9]* [0-9]+(:[0-9]+)?$")) {
 			set(line, block);
-		} else if (line.matches("^TP -?[0-9]+,[0-9]+,-?[0-9]+$") && player != null) {
+		} else if (line.matches("^TP? -?[0-9]+,[0-9]+,-?[0-9]+$") && player != null) {
 			tp(line, player);
-		} else if (line.matches("^TPA -?[0-9]+,[0-9]+,-?[0-9]+$")) {
+		} else if (line.matches("^T(A|PA) -?[0-9]+,[0-9]+,-?[0-9]+$")) {
 			tpAll(line, block);
+		} else if (line.matches("^T(R|PR) -?[0-9]*,-?[0-9]*,-?[0-9]*$") && player != null) {
+			tpRelative(line, block, player);
 		} else if (line.matches("^CP -?[0-9]+,[0-9]+,-?[0-9]+$")) {
 			checkpoint(line, block);
-		} else if (line.matches("^EXP [0-9]+$")) {
+		} else if (line.matches("^GLD -?[0-9]*,-?[0-9]*,-?[0-9]* [0-9]+")) {
+			dropGold(line, block);
+		} else if (line.matches("^EXP [0-9]+$") && player != null) {
 			giveExp(line, player);
 		} else if (line.matches("^EXPA [0-9]+$")) {
 			giveExpAll(line, block);
@@ -47,18 +56,22 @@ public class SignExecutor {
 			spawnMob(line, block);
 		} else if (line.matches("^EXE -?[0-9]*,-?[0-9]*,-?[0-9]*$")) {
 			exe(line, block, player);
-		} else if (line.matches("^EXE -?[0-9]*,-?[0-9]*,-?[0-9]* [0-9]+(T|S|M)?$")) {
+		} else if (line.matches("^EXED -?[0-9]*,-?[0-9]*,-?[0-9]* [0-9]+(T|S|M)?$")) {
 			exeDelayed(line, block, player);
 		} else if (line.matches("^SAY -?[0-9]*,-?[0-9]*,-?[0-9]*$") && player != null) {
 			say(line, block, player);
 		} else if (line.matches("^SAYA -?[0-9]*,-?[0-9]*,-?[0-9]*$")) {
 			sayAll(line, block);
+		} else if (line.matches("^EFF -?[0-9]*,-?[0-9]*,-?[0-9]* (Blz|Bow|C1|C2|Dr|End|Ext|Gh1|Gh2|Mob|Po|Rc|Sm|Bk)( [0-9]+)?$")) {
+			playEffect(line, block);
 		} else if (line.matches("^CLI -?[0-9]*,-?[0-9]*,-?[0-9]*$")) {
 			consoleCommand(line, block, player);
 		} else if (line.matches("^WEA (rain|sun)$")) {
 			weather(line, block);
 		} else if (line.matches("^TIME [0-9]+$")) {
 			time(line, block);
+		} else if (line.equals("DISABLE")) {
+			disable(line, block);
 		} else if (line.equals("END")) {
 			end(line, block);
 		}
@@ -93,7 +106,7 @@ public class SignExecutor {
 		int y = Integer.parseInt(coords[1]);
 		int z = Integer.parseInt(coords[2]);
 		
-		player.teleport(new Location(player.getWorld(), x, y, z), TeleportCause.PLUGIN);
+		player.teleport(new Location(player.getWorld(), x+.5, y+.5, z+.5), TeleportCause.PLUGIN);
 	}
 	
 	private static void tpAll(String line, Block block) {
@@ -104,10 +117,22 @@ public class SignExecutor {
 		int y = Integer.parseInt(coords[1]);
 		int z = Integer.parseInt(coords[2]);
 		
-		Location loc = new Location(block.getWorld(), x, y, z);
+		Location loc = new Location(block.getWorld(), x+.5, y+.5, z+.5);
 		for (Player player : block.getWorld().getPlayers()) {
 			player.teleport(loc, TeleportCause.PLUGIN);
 		}
+	}
+	
+	private static void tpRelative(String line, Block block, Player player) {
+		String[] args = line.split(" ");
+		String[] coords = args[1].split(",", 3);
+		
+		int x = coords[0].isEmpty() ? 0 : Integer.parseInt(coords[0]);
+		int y = coords[1].isEmpty() ? 0 : Integer.parseInt(coords[1]);
+		int z = coords[2].isEmpty() ? 0 : Integer.parseInt(coords[2]);
+		
+		Location loc = block.getRelative(x, y, z).getLocation().add(.5, .5, .5);
+		player.teleport(loc, TeleportCause.PLUGIN);		
 	}
 	
 	private static void checkpoint(String line, Block block) {
@@ -123,6 +148,20 @@ public class SignExecutor {
 			Location loc = new Location(block.getWorld(), x, y, z);
 			instance.setRespawn(loc);
 		}
+	}
+	
+	private static void dropGold(String line, Block block) {
+		String[] args = line.split(" ");
+		String[] coords = args[1].split(",", 3);
+		
+		int x = coords[0].isEmpty() ? 0 : Integer.parseInt(coords[0]);
+		int y = coords[1].isEmpty() ? 0 : Integer.parseInt(coords[1]);
+		int z = coords[2].isEmpty() ? 0 : Integer.parseInt(coords[2]);
+		
+		int amt = Integer.parseInt(args[2]);
+		
+		Location loc = block.getRelative(x, y, z).getLocation().add(.5, .5, .5);
+		WorldLoader.plugin.dropGold(loc, amt);
 	}
 	
 	private static void giveExp(String line, Player player) {
@@ -224,7 +263,10 @@ public class SignExecutor {
 		final Block sign = block.getWorld().getBlockAt(new Location(block.getWorld(), block.getX() + x, block.getY() + y, block.getZ() + z));
 		Bukkit.getScheduler().scheduleSyncDelayedTask(WorldLoader.plugin, new Runnable() {
 			public void run() {
-				executeSign(sign, player);
+				if (sign.getWorld() != null) {
+					System.out.println("executing delayed...");
+					executeSign(sign, player);					
+				}
 			}
 		}, Integer.parseInt(args[2]));
 	}
@@ -279,6 +321,57 @@ public class SignExecutor {
 		}
 	}
 	
+	private static void playEffect(String line, Block block) {
+		String[] args = line.split(" ");
+		String[] coords = args[1].split(",", 3);
+		
+		int x = coords[0].isEmpty() ? 0 : Integer.parseInt(coords[0]);
+		int y = coords[1].isEmpty() ? 0 : Integer.parseInt(coords[1]);
+		int z = coords[2].isEmpty() ? 0 : Integer.parseInt(coords[2]);
+		
+		Location loc = block.getRelative(x, y, z).getLocation();
+		
+		Effect effect = null;
+		if (args[2].equals("Blz")) {
+			effect = Effect.BLAZE_SHOOT;
+		} else if (args[2].equals("Bow")) {
+			effect = Effect.BOW_FIRE;
+		} else if (args[2].equals("C1")) {
+			effect = Effect.CLICK1;
+		} else if (args[2].equals("C2")) {
+			effect = Effect.CLICK2;
+		} else if (args[2].equals("Dr")) {
+			effect = Effect.DOOR_TOGGLE;
+		} else if (args[2].equals("End")) {
+			effect = Effect.ENDER_SIGNAL;
+		} else if (args[2].equals("Ext")) {
+			effect = Effect.EXTINGUISH;
+		} else if (args[2].equals("Gh1")) {
+			effect = Effect.GHAST_SHOOT;
+		} else if (args[2].equals("Gh2")) {
+			effect = Effect.GHAST_SHRIEK;
+		} else if (args[2].equals("Mob")) {
+			effect = Effect.MOBSPAWNER_FLAMES;
+		} else if (args[2].equals("Po")) {
+			effect = Effect.POTION_BREAK;
+		} else if (args[2].equals("Rc")) {
+			effect = Effect.RECORD_PLAY;
+		} else if (args[2].equals("Sm")) {
+			effect = Effect.SMOKE;
+		} else if (args[2].equals("Bk")) {
+			effect = Effect.STEP_SOUND;
+		}
+		
+		int data = 0;
+		if (args.length > 3) {
+			data = Integer.parseInt(args[3]);
+		}
+		
+		System.out.println(effect + " " + data);
+		
+		block.getWorld().playEffect(loc, effect, data);
+	}
+	
 	private static void consoleCommand(String line, Block block, Player player) {
 		String[] args = line.split(" ");
 		String[] coords = args[1].split(",", 3);
@@ -324,11 +417,18 @@ public class SignExecutor {
 		block.getWorld().setTime(time);
 	}
 	
+	private static void disable(String line, Block block) {
+		Sign sign = (Sign)block.getState();
+		sign.setLine(0, "");
+		sign.update();
+	}
+	
 	private static void end(String line, Block block) {
 		WorldInstance instance = WorldLoader.plugin.getWorldInstance(block.getWorld());
 		if (instance != null) {
-			instance.unloadWorld(true);
+			WorldLoader.plugin.killInstance(instance, true);
 		}
 	}
+
 	
 }
