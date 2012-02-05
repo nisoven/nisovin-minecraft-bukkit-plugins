92f90cd4847ea946378616a777b8deeb0f4d2ea0
nisovin@gmail.com <nisovin@gmail.com@3b5476c5-0415-9417-c352-9b123fd710cf>
2012-02-05 20:39:44 +0000

    MS: added fire-tick-damage and prevent-immunity to combust spell

--- trunk/MagicSpells/src/com/nisovin/magicspells/spells/targeted/CombustSpell.java
+++ trunk/MagicSpells/src/com/nisovin/magicspells/spells/targeted/CombustSpell.java
@@ -1,11 +1,16 @@
 package com.nisovin.magicspells.spells.targeted;
 
+import java.util.HashMap;
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
+import com.nisovin.magicspells.MagicSpells;
 import com.nisovin.magicspells.spells.TargetedSpell;
 import com.nisovin.magicspells.util.MagicConfig;
 
@@ -13,24 +18,30 @@ public class CombustSpell extends TargetedSpell {
 	
 	private boolean targetPlayers;
 	private int fireTicks;
+	private int fireTickDamage;
+	private boolean preventImmunity;
 	private boolean obeyLos;
 	private boolean checkPlugins;
 	private String strNoTarget;
 	
+	private HashMap<Integer, CombustData> combusting = new HashMap<Integer, CombustData>();
+	
 	public CombustSpell(MagicConfig config, String spellName) {
 		super(config, spellName);
 		
-		targetPlayers = config.getBoolean("spells." + spellName + ".target-players", false);
-		fireTicks = config.getInt("spells." + spellName + ".fire-ticks", 100);
-		obeyLos = config.getBoolean("spells." + spellName + ".obey-los", true);
-		checkPlugins = config.getBoolean("spells." + spellName + ".check-plugins", true);
-		strNoTarget = config.getString("spells." + spellName + ".str-no-target", "");
+		targetPlayers = getConfigBoolean("target-players", false);
+		fireTicks = getConfigInt("fire-ticks", 100);
+		fireTickDamage = getConfigInt("fire-tick-damage", 1);
+		preventImmunity = getConfigBoolean("prevent-immunity", true);
+		obeyLos = getConfigBoolean("obey-los", true);
+		checkPlugins = getConfigBoolean("check-plugins", true);
+		strNoTarget = getConfigString("str-no-target", "");
 	}
 	
 	@Override
 	public PostCastAction castSpell(Player player, SpellCastState state, float power, String[] args) {
 		if (state == SpellCastState.NORMAL) {
-			LivingEntity target = getTargetedEntity(player, range>0?range:100, targetPlayers, obeyLos);
+			final LivingEntity target = getTargetedEntity(player, range>0?range:100, targetPlayers, obeyLos);
 			if (target == null) {
 				sendMessage(player, strNoTarget);
 				fizzle(player);
@@ -46,10 +57,52 @@ public class CombustSpell extends TargetedSpell {
 						return PostCastAction.ALREADY_HANDLED;
 					}
 				}
-				target.setFireTicks(Math.round(fireTicks*power));
+				int duration = Math.round(fireTicks*power);
+				combusting.put(target.getEntityId(), new CombustData(power, target.getWorld().getFullTime() + duration));
+				target.setFireTicks(duration);
+				Bukkit.getScheduler().scheduleSyncDelayedTask(MagicSpells.plugin, new Runnable() {
+					public void run() {
+						CombustData data = combusting.get(target.getEntityId());
+						if (data != null) {
+							if (target.getWorld().getFullTime() > data.expires) {
+								combusting.remove(target.getEntityId());
+							}
+						}
+					}
+				}, duration+2);
+				
 				// TODO: manually send messages with replacements
 			}
 		}
 		return PostCastAction.HANDLE_NORMALLY;
+	}
+	
+	@EventHandler
+	public void onEntityDamage(final EntityDamageEvent event) {
+		if (event.isCancelled() || event.getCause() != DamageCause.FIRE_TICK) return;
+		
+		CombustData data = combusting.get(event.getEntity().getEntityId());
+		if (data != null) {
+			if (event.getEntity().getWorld().getFullTime() <= data.expires) { 
+				event.setDamage(Math.round(fireTickDamage * data.power));
+			}
+			if (preventImmunity) {
+				Bukkit.getScheduler().scheduleSyncDelayedTask(MagicSpells.plugin, new Runnable() {
+					public void run() {
+						((LivingEntity)event.getEntity()).setNoDamageTicks(0);
+					}
+				}, 0);
+			}
+		}
+	}
+	
+	private class CombustData {
+		float power;
+		long expires;
+		
+		CombustData(float power, long expires) {
+			this.power = power;
+			this.expires = expires;
+		}
 	}
 }
