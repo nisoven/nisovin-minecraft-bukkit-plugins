7c82be2c0d9e13a314cd6b440dad3927ac26698c
nisovin@gmail.com <nisovin@gmail.com@3b5476c5-0415-9417-c352-9b123fd710cf>
2012-02-26 19:37:45 +0000

    MS: Added dont-drop and prevent-theft options to disarm

--- trunk/MagicSpells/src/com/nisovin/magicspells/spells/targeted/DisarmSpell.java
+++ trunk/MagicSpells/src/com/nisovin/magicspells/spells/targeted/DisarmSpell.java
@@ -1,13 +1,19 @@
 package com.nisovin.magicspells.spells.targeted;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.inventory.ItemStack;
 
+import com.nisovin.magicspells.MagicSpells;
 import com.nisovin.magicspells.spells.TargetedEntitySpell;
 import com.nisovin.magicspells.util.MagicConfig;
 
@@ -15,11 +21,15 @@ public class DisarmSpell extends TargetedEntitySpell {
 
 	private List<Integer> disarmable;
 	private int disarmDuration;
+	private boolean dontDrop;
+	private boolean preventTheft;
 	private boolean obeyLos;
 	
 	private String strNoTarget;
 	private String strInvalidItem;
 	private String strCastTarget;
+	
+	private HashMap<Item, Player> disarmedItems;
 	
 	public DisarmSpell(MagicConfig config, String spellName) {
 		super(config, spellName);
@@ -29,10 +39,17 @@ public class DisarmSpell extends TargetedEntitySpell {
 		disarmable = getConfigIntList("disarmable-items", disarmable);
 		
 		disarmDuration = getConfigInt("disarm-duration", 100);
+		dontDrop = getConfigBoolean("dont-drop", false);
+		preventTheft = getConfigBoolean("prevent-theft", true);
 		obeyLos = getConfigBoolean("obey-los", true);
 		strNoTarget = getConfigString("str-no-target", "No target found.");
 		strInvalidItem = getConfigString("str-invalid-item", "Your target could not be disarmed.");
 		strCastTarget = getConfigString("str-cast-target", "%a has disarmed you.");
+		
+		if (dontDrop) preventTheft = false;
+		if (preventTheft) {
+			disarmedItems = new HashMap<Item, Player>();
+		}
 	}
 
 	@Override
@@ -63,13 +80,41 @@ public class DisarmSpell extends TargetedEntitySpell {
 		return PostCastAction.HANDLE_NORMALLY;
 	}
 	
-	private boolean disarm(Player target) {
-		ItemStack inHand = target.getItemInHand();
+	private boolean disarm(final Player target) {
+		final ItemStack inHand = target.getItemInHand();
 		if (disarmable.contains(inHand.getTypeId())) {
-			// drop item
-			target.setItemInHand(null);
-			Item item = target.getWorld().dropItemNaturally(target.getLocation(), inHand.clone());
-			item.setPickupDelay(disarmDuration);
+			if (dontDrop) {
+				// hide item
+				target.setItemInHand(null);
+				Bukkit.getScheduler().scheduleSyncDelayedTask(MagicSpells.plugin, new Runnable() {
+					public void run() {
+						// give the item back
+						if (target.getItemInHand() == null || target.getItemInHand().getType() == Material.AIR) {
+							// put back in hand
+							target.setItemInHand(inHand);
+						} else {
+							// hand is full
+							int slot = target.getInventory().firstEmpty();
+							if (slot >= 0) {
+								// put in first available slot
+								target.getInventory().setItem(slot, inHand);
+							} else {
+								// no slots available, drop at feet
+								Item item = target.getWorld().dropItem(target.getLocation(), inHand);
+								item.setPickupDelay(0);
+							}
+						}
+					}
+				}, disarmDuration);
+			} else {
+				// drop item
+				target.setItemInHand(null);
+				Item item = target.getWorld().dropItemNaturally(target.getLocation(), inHand.clone());
+				item.setPickupDelay(disarmDuration);
+				if (preventTheft) {
+					disarmedItems.put(item, target);
+				}
+			}
 			return true;
 		} else {
 			return false;
@@ -82,6 +127,20 @@ public class DisarmSpell extends TargetedEntitySpell {
 			return disarm((Player)target);
 		} else {
 			return false;
+		}
+	}
+	
+	@EventHandler
+	public void onItemPickup(PlayerPickupItemEvent event) {
+		if (!preventTheft || event.isCancelled()) return;
+		
+		Item item = event.getItem();
+		if (disarmedItems.containsKey(item)) {
+			if (disarmedItems.get(item).equals(event.getPlayer())) {
+				disarmedItems.remove(item);
+			} else {
+				event.setCancelled(true);
+			}
 		}
 	}
 
