5520504a079d96cf582cdc3f203cedfd2d1aa45f
nisovin@gmail.com <nisovin@gmail.com@3b5476c5-0415-9417-c352-9b123fd710cf>
2012-03-20 18:42:18 +0000

    YAPP: added commands

--- trunk/YAPP/src/com/nisovin/yapp/User.java
+++ trunk/YAPP/src/com/nisovin/yapp/User.java
@@ -4,12 +4,21 @@ import org.bukkit.entity.Player;
 
 public class User extends PermissionContainer {
 
+	private String realName;
+	
 	public User(Player player) {
 		super(player.getName().toLowerCase(), "player");
+		this.realName = player.getName();
 	}
 	
 	public User(String player) {
 		super(player.toLowerCase(), "player");
+		this.realName = player;
+	}
+	
+	@Override
+	public String getName() {
+		return realName;
 	}
 	
 }
