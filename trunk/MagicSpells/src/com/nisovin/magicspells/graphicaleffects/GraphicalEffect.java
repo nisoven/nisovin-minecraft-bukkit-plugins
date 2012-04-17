043e367f21a4f8d688a030ae73b8cc50c351d74d
nisovin@gmail.com <nisovin@gmail.com@3b5476c5-0415-9417-c352-9b123fd710cf>
2012-04-17 00:54:59 +0000

    MS: added hearts effect

--- trunk/MagicSpells/src/com/nisovin/magicspells/graphicaleffects/GraphicalEffect.java
+++ trunk/MagicSpells/src/com/nisovin/magicspells/graphicaleffects/GraphicalEffect.java
@@ -67,6 +67,7 @@ public abstract class GraphicalEffect {
 		effects.put("cloud", new CloudEffect());
 		effects.put("ender", new EnderSignalEffect());
 		effects.put("explosion", new ExplosionEffect());
+		effects.put("hearts", new HeartsEffect());
 		effects.put("itemspray", new ItemSprayEffect());
 		effects.put("lightning", new LightningEffect());
 		effects.put("potion", new PotionEffect());
