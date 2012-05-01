2199af0b23ae9ddfcafbb01dd5877c1e5733540b
nisovin <nisovin@gmail.com>
2012-05-01 16:24:40 +0000

    MS: added cancel-on-damage option to windwalk

--- MagicSpells/src/com/nisovin/magicspells/spells/buff/WindwalkSpell.java
+++ MagicSpells/src/com/nisovin/magicspells/spells/buff/WindwalkSpell.java
@@ -8,9 +8,13 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerPortalEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.util.Vector;
 
 import com.nisovin.magicspells.MagicSpells;
@@ -23,6 +27,7 @@ public class WindwalkSpell extends BuffSpell {
     private boolean cancelOnLand;
 	private boolean cancelOnLogout;
 	private boolean cancelOnTeleport;
+	private boolean cancelOnDamage;
 	
 	private HashSet<Player> flyers;
 	private HashMap<Player, Integer> tasks;
@@ -34,10 +39,30 @@ public class WindwalkSpell extends BuffSpell {
         cancelOnLand = getConfigBoolean("cancel-on-land", true);
 		cancelOnLogout = getConfigBoolean("cancel-on-logout", true);
 		cancelOnTeleport = getConfigBoolean("cancel-on-teleport", true);
+		cancelOnDamage = getConfigBoolean("cancel-on-damage", false);
 		
 		flyers = new HashSet<Player>();
 		if (useCostInterval > 0) {
 			tasks = new HashMap<Player, Integer>();
+		}
+	}
+	
+	@Override
+	public void initialize() {
+		super.initialize();
+		
+		PluginManager pm = Bukkit.getPluginManager();
+		if (cancelOnLand) {
+			pm.registerEvents(new SneakListener(), MagicSpells.plugin);
+		}
+		if (cancelOnLogout) {
+			pm.registerEvents(new QuitListener(), MagicSpells.plugin);
+		}
+		if (cancelOnTeleport) {
+			pm.registerEvents(new TeleportListener(), MagicSpells.plugin);
+		}
+		if (cancelOnDamage) {
+			pm.registerEvents(new DamageListener(), MagicSpells.plugin);
 		}
 	}
 
@@ -75,28 +100,47 @@ public class WindwalkSpell extends BuffSpell {
 		return PostCastAction.HANDLE_NORMALLY;
 	}
     
-    @EventHandler(priority=EventPriority.MONITOR)
-    public void onPlayerToggleSneak(PlayerToggleSneakEvent event) {
-        if (cancelOnLand && flyers.contains(event.getPlayer())) {
-            if (event.getPlayer().getLocation().subtract(0,1,0).getBlock().getType() != Material.AIR) {
-                turnOff(event.getPlayer());
-            }
-        }
-    }
+	public class SneakListener implements Listener {
+	    @EventHandler(priority=EventPriority.MONITOR)
+	    public void onPlayerToggleSneak(PlayerToggleSneakEvent event) {
+	        if (flyers.contains(event.getPlayer())) {
+	            if (event.getPlayer().getLocation().subtract(0,1,0).getBlock().getType() != Material.AIR) {
+	                turnOff(event.getPlayer());
+	            }
+	        }
+	    }
+	}
 
-	@EventHandler(priority=EventPriority.MONITOR)
-	public void onPlayerQuit(PlayerQuitEvent event) {
-		if (cancelOnLogout) {
+	public class QuitListener implements Listener {
+		@EventHandler(priority=EventPriority.MONITOR)
+		public void onPlayerQuit(PlayerQuitEvent event) {
 			turnOff(event.getPlayer());
 		}
 	}
 
-	@EventHandler(priority=EventPriority.MONITOR)
-	public void onPlayerTeleport(PlayerTeleportEvent event) {
-		if (event.isCancelled()) return;
-		if (cancelOnTeleport && flyers.contains(event.getPlayer())) {
-			if (!event.getFrom().getWorld().getName().equals(event.getTo().getWorld().getName()) || event.getFrom().toVector().distanceSquared(event.getTo().toVector()) > 50*50) {
+	public class TeleportListener implements Listener {
+		@EventHandler(priority=EventPriority.MONITOR, ignoreCancelled=true)
+		public void onPlayerTeleport(PlayerTeleportEvent event) {
+			if (flyers.contains(event.getPlayer())) {
+				if (!event.getFrom().getWorld().getName().equals(event.getTo().getWorld().getName()) || event.getFrom().toVector().distanceSquared(event.getTo().toVector()) > 50*50) {
+					turnOff(event.getPlayer());
+				}
+			}
+		}
+		
+		@EventHandler(priority=EventPriority.MONITOR, ignoreCancelled=true)
+		public void onPlayerPortal(PlayerPortalEvent event) {
+			if (flyers.contains(event.getPlayer())) {
 				turnOff(event.getPlayer());
+			}
+		}
+	}
+	
+	public class DamageListener implements Listener {
+		@EventHandler(priority=EventPriority.MONITOR, ignoreCancelled=true)
+		public void onPlayerDamage(EntityDamageEvent event) {
+			if (event.getEntity() instanceof Player && flyers.contains((Player)event.getEntity())) {
+				turnOff((Player)event.getEntity());
 			}
 		}
 	}
