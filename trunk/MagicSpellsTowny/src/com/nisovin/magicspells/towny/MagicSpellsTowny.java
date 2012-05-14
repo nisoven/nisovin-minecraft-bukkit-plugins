cb58af00e75e5624a33922db3f819f8b2ee30a7d
nisovin@gmail.com <nisovin@gmail.com@3b5476c5-0415-9417-c352-9b123fd710cf>
2012-05-14 17:31:41 +0000

    MSTowny: updated for new Towny version

--- trunk/MagicSpellsTowny/src/com/nisovin/magicspells/towny/MagicSpellsTowny.java
+++ trunk/MagicSpellsTowny/src/com/nisovin/magicspells/towny/MagicSpellsTowny.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Set;
 
 import org.bukkit.configuration.Configuration;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
@@ -16,20 +15,13 @@ import com.nisovin.magicspells.MagicSpells;
 import com.nisovin.magicspells.Spell;
 import com.nisovin.magicspells.events.SpellCastEvent;
 import com.nisovin.magicspells.events.SpellTargetEvent;
-import com.palmergames.bukkit.towny.NotRegisteredException;
-import com.palmergames.bukkit.towny.Towny;
-import com.palmergames.bukkit.towny.TownyException;
-import com.palmergames.bukkit.towny.TownySettings;
+import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
 import com.palmergames.bukkit.towny.object.Coord;
-import com.palmergames.bukkit.towny.object.TownBlock;
-import com.palmergames.bukkit.towny.object.TownBlockType;
 import com.palmergames.bukkit.towny.object.TownyUniverse;
 import com.palmergames.bukkit.towny.object.TownyWorld;
-import com.palmergames.bukkit.towny.object.WorldCoord;
+import com.palmergames.bukkit.towny.utils.CombatUtil;
 
 public class MagicSpellsTowny extends JavaPlugin implements Listener {
-
-	private Towny towny;
 	
 	private Set<Spell> disallowedInTowns = new HashSet<Spell>();
 	
@@ -57,7 +49,6 @@ public class MagicSpellsTowny extends JavaPlugin implements Listener {
 		
 		Plugin townyPlugin = getServer().getPluginManager().getPlugin("Towny");
 		if (townyPlugin != null) {
-			towny = (Towny)townyPlugin;
 			getServer().getPluginManager().registerEvents(this, this);
 			getLogger().info("All systems go");
 		}
@@ -65,14 +56,8 @@ public class MagicSpellsTowny extends JavaPlugin implements Listener {
 	
 	@EventHandler(ignoreCancelled=true)
 	public void onSpellTarget(SpellTargetEvent event) {
-		if (event.getTarget() instanceof Player) {
-			try {
-				TownyWorld world = TownyUniverse.getDataSource().getWorld(event.getCaster().getWorld().getName());
-				if (preventDamageCall(world, event.getCaster(), (Player)event.getTarget())) {
-					event.setCancelled(true);
-				}
-			} catch (NotRegisteredException e) {
-			}
+		if (CombatUtil.preventDamageCall(event.getCaster(), event.getTarget())) {
+			event.setCancelled(true);
 		}
 	}
 	
@@ -90,49 +75,6 @@ public class MagicSpellsTowny extends JavaPlugin implements Listener {
 			} catch (NotRegisteredException e) {
 			}
 		}
-	}
-
-	public boolean preventDamageCall(TownyWorld world, Player ap, Player bp) {
-		// World using Towny
-		if (!world.isUsingTowny())
-			return false;
-
-		Coord coord = Coord.parseCoord(bp);
-
-		if (world.isWarZone(coord))
-			return false;
-
-		if (preventFriendlyFire(ap, bp))
-			return true;
-
-		try {
-			// Check Town PvP status
-			TownBlock townblock = world.getTownBlock(coord);
-			if (!townblock.getTown().isPVP() && !world.isForcePVP() && !townblock.getPermissions().pvp) {
-				return true;
-			}
-		} catch (NotRegisteredException e) {
-			// Not in a town
-			if ((!world.isPVP()) && (!world.isForcePVP()))
-				return true;
-		}
-
-		return false;
-	}
-
-	public boolean preventFriendlyFire(Player a, Player b) {
-		TownyUniverse universe = towny.getTownyUniverse();
-		if (!TownySettings.getFriendlyFire() && universe.isAlly(a.getName(), b.getName())) {
-			try {
-				TownyWorld world = TownyUniverse.getDataSource().getWorld(b.getWorld().getName());
-				TownBlock townBlock = new WorldCoord(world, Coord.parseCoord(b)).getTownBlock();
-				if (!townBlock.getType().equals(TownBlockType.ARENA))
-					return true;
-			} catch (TownyException x) {
-				return true;
-			}
-		}
-		return false;
 	}
 	
 }
