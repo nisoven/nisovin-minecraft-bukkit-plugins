a311af2b245a116cc47350ad61504dd607996f77
nisovin <nisovin@gmail.com>
2012-05-21 17:56:41 +0000

    MSCM: release version

--- MagicSpellsCastModifiers/src/com/nisovin/magicspells/castmodifiers/Modifier.java
+++ MagicSpellsCastModifiers/src/com/nisovin/magicspells/castmodifiers/Modifier.java
@@ -2,6 +2,7 @@ package com.nisovin.magicspells.castmodifiers;
 
 import org.bukkit.entity.Player;
 
+import com.nisovin.magicspells.events.ManaChangeEvent;
 import com.nisovin.magicspells.events.SpellCastEvent;
 
 public class Modifier {
@@ -43,13 +44,34 @@ public class Modifier {
 		return true;
 	}
 	
+	public boolean apply(ManaChangeEvent event) {
+		Player player = event.getPlayer();
+		boolean check = condition.check(player, conditionVar);
+		if (check == false && type == ModifierType.REQUIRED) {
+			event.setNewAmount(event.getOldAmount());
+			return false;
+		} else if (check == true && type == ModifierType.DENIED) {
+			event.setNewAmount(event.getOldAmount());
+			return false;
+		} else if (type == ModifierType.POWER) {
+			int gain = event.getNewAmount() - event.getOldAmount();
+			gain = (int)(gain * Float.parseFloat(modifierVar));
+			int newAmt = event.getOldAmount() + gain;
+			if (newAmt > event.getMaxMana()) newAmt = event.getMaxMana();
+			event.setNewAmount(newAmt);
+		}
+		return true;
+	}
+	
 	private ModifierType getTypeByName(String name) {
 		if (name.equalsIgnoreCase("required") || name.equalsIgnoreCase("require")) {
 			return ModifierType.REQUIRED;
 		} else if (name.equalsIgnoreCase("denied") || name.equalsIgnoreCase("deny")) {
 			return ModifierType.DENIED;
-		} else if (name.equalsIgnoreCase("power") || name.equalsIgnoreCase("empower")) {
+		} else if (name.equalsIgnoreCase("power") || name.equalsIgnoreCase("empower") || name.equalsIgnoreCase("multiply")) {
 			return ModifierType.POWER;
+		} else if (name.equalsIgnoreCase("cooldown")) {
+			return ModifierType.COOLDOWN;
 		} else {
 			return null;
 		}
