404950d8d4570567bd1569d39cd66d7d18712d86
nisovin@gmail.com <nisovin@gmail.com@3b5476c5-0415-9417-c352-9b123fd710cf>
2012-06-19 22:23:05 +0000

    MS: fixed volley, added shoot-interval option

--- trunk/MagicSpells/src/com/nisovin/magicspells/spells/targeted/VolleySpell.java
+++ trunk/MagicSpells/src/com/nisovin/magicspells/spells/targeted/VolleySpell.java
@@ -1,5 +1,6 @@
 package com.nisovin.magicspells.spells.targeted;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -7,6 +8,7 @@ import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
+import com.nisovin.magicspells.MagicSpells;
 import com.nisovin.magicspells.spells.TargetedLocationSpell;
 import com.nisovin.magicspells.util.MagicConfig;
 
@@ -15,6 +17,7 @@ public class VolleySpell extends TargetedLocationSpell {
 	private int arrows;
 	private int speed;
 	private int spread;
+	private int shootInterval;
 	
 	public VolleySpell(MagicConfig config, String spellName) {
 		super(config, spellName);
@@ -22,6 +25,7 @@ public class VolleySpell extends TargetedLocationSpell {
 		arrows = getConfigInt("arrows", 10);
 		speed = getConfigInt("speed", 20);
 		spread = getConfigInt("spread", 150);
+		shootInterval = getConfigInt("shoot-interval", 0);
 	}
 	
 	@Override
@@ -46,12 +50,17 @@ public class VolleySpell extends TargetedLocationSpell {
 	private void volley(Player player, Location target, float power) {
 		Location spawn = player.getLocation();
 		spawn.setY(spawn.getY()+3);
-		
 		Vector v = target.toVector().subtract(spawn.toVector()).normalize();
-		int arrows = Math.round(this.arrows*power);
-		for (int i = 0; i < arrows; i++) {
-			Arrow a = player.getWorld().spawnArrow(spawn, v, (speed/10.0F), (spread/10.0F));
-			a.setShooter(player);
+		
+		if (shootInterval <= 0) {
+			int arrows = Math.round(this.arrows*power);
+			for (int i = 0; i < arrows; i++) {
+				Arrow a = player.getWorld().spawnArrow(spawn, v, (speed/10.0F), (spread/10.0F));
+				a.setVelocity(a.getVelocity());
+				a.setShooter(player);
+			}
+		} else {
+			new ArrowShooter(player, spawn, v);
 		}
 		
 		playSpellEffects(player, target);
@@ -61,6 +70,32 @@ public class VolleySpell extends TargetedLocationSpell {
 	public boolean castAtLocation(Player caster, Location target, float power) {
 		volley(caster, target, power);
 		return true;
+	}
+	
+	private class ArrowShooter implements Runnable {
+		Player player;
+		Location spawn;
+		Vector dir;
+		int count;
+		int taskId;
+		
+		ArrowShooter(Player player, Location spawn, Vector dir) {
+			this.player = player;
+			this.spawn = spawn;
+			this.dir = dir;
+			this.count = 0;
+			this.taskId = Bukkit.getScheduler().scheduleSyncRepeatingTask(MagicSpells.plugin, this, 0, shootInterval);
+		}
+		
+		@Override
+		public void run() {
+			Arrow a = player.getWorld().spawnArrow(spawn, dir, (speed/10.0F), (spread/10.0F));
+			a.setVelocity(a.getVelocity());
+			a.setShooter(player);
+			if (++count >= arrows) {
+				Bukkit.getScheduler().cancelTask(taskId);
+			}
+		}
 	}
 
 }
