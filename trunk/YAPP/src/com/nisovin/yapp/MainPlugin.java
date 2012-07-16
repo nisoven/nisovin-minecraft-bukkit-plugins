c82ead35569ca4390e6c3c6c38f2ccfba1c9edaa
nisovin@gmail.com <nisovin@gmail.com@3b5476c5-0415-9417-c352-9b123fd710cf>
2012-07-16 22:39:07 +0000

    YAPP: a couple tweaks

--- trunk/YAPP/src/com/nisovin/yapp/MainPlugin.java
+++ trunk/YAPP/src/com/nisovin/yapp/MainPlugin.java
@@ -5,6 +5,7 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -99,7 +100,7 @@ public class MainPlugin extends JavaPlugin {
 		}
 		
 		// get promotion ladders
-		ladders = new HashMap<String, List<Group>>();
+		ladders = new LinkedHashMap<String, List<Group>>();
 		Set<String> keys = config.getKeys("ladders");
 		if (keys != null) {
 			for (String key : keys) {
