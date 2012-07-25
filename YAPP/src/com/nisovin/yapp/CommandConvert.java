2d49eb96388b7e84a76fac9bb049cd8a6a76647c
nisovin <nisovin@gmail.com>
2012-07-25 06:23:23 +0000

    YAPP: added permbukkit conversion script

--- YAPP/src/com/nisovin/yapp/CommandConvert.java
+++ YAPP/src/com/nisovin/yapp/CommandConvert.java
@@ -21,6 +21,8 @@ public class CommandConvert implements CommandExecutor {
 				sender.sendMessage("You must specify a plugin to convert from (pex or permbukkit)");
 			} else if (args[0].equalsIgnoreCase("pex")) {
 				convertFromPex();
+			} else if (args[0].equalsIgnoreCase("permbukkit")) {
+				convertFromPermissionsBukkit();
 			}
 		}
 		
@@ -192,5 +194,148 @@ public class CommandConvert implements CommandExecutor {
 		
 		MainPlugin.yapp.saveAll();
 	}
+
+	private void convertFromPermissionsBukkit() {
+		File file = new File("plugins/PermissionsBukkit/config.yml");
+		if (!file.exists()) return;
+		
+		YamlConfiguration config = new YamlConfiguration();
+		try {
+			config.load(file);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		// get groups
+		ConfigurationSection groupsSection = config.getConfigurationSection("groups");
+		HashMap<Group, HashMap<String, List<String>>> allInheritedGroups = new HashMap<Group, HashMap<String, List<String>>>(); // gotta handle this later
+		if (groupsSection != null) {
+			Set<String> groupKeys = groupsSection.getKeys(false);
+			for (String groupName : groupKeys) {
+				// get group
+				ConfigurationSection groupSection = groupsSection.getConfigurationSection(groupName);
+				Group group = MainPlugin.getGroup(groupName);
+				if (group == null) group = MainPlugin.newGroup(groupName);
+				
+				// prepare inherited group storage
+				HashMap<String, List<String>> inheritedGroups = new HashMap<String, List<String>>();
+				allInheritedGroups.put(group, inheritedGroups);
+				
+				// get group's inherited groups
+				List<String> inheritedGroupNames = groupSection.getStringList("inheritance");
+				if (inheritedGroupNames != null && inheritedGroupNames.size() > 0) {
+					inheritedGroups.put(null, inheritedGroupNames);
+				}
+				
+				// get group's permissions and add to group
+				ConfigurationSection permissionsSection = groupSection.getConfigurationSection("permissions");
+				if (permissionsSection != null) {
+					Set<String> permissionKeys = permissionsSection.getKeys(true);
+					for (String permission : permissionKeys) {
+						if (permissionsSection.isBoolean(permission)) {
+							boolean value = permissionsSection.getBoolean(permission, true);
+							group.addPermission((value ? "" : "-") + permission);
+						}
+					}
+				}
+				
+				// get worlds
+				if (groupSection.contains("worlds")) {
+					ConfigurationSection worldsSection = groupSection.getConfigurationSection("worlds");
+					Set<String> worldNames = worldsSection.getKeys(false);
+					for (String worldName : worldNames) {
+						ConfigurationSection worldSection = worldsSection.getConfigurationSection(worldName);						
+						
+						// get world permissions						
+						Set<String> worldPermissions = worldSection.getKeys(true);
+						if (worldPermissions != null) {
+							for (String permission : worldPermissions) {
+								if (worldSection.isBoolean(permission)) {
+									boolean value = worldSection.getBoolean(permission, true);
+									group.addPermission(worldName, (value ? "" : "-") + permission);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		// add group inherited groups
+		for (Group group : allInheritedGroups.keySet()) {
+			HashMap<String, List<String>> inheritedGroupsByWorld = allInheritedGroups.get(group);
+			for (String world : inheritedGroupsByWorld.keySet()) {
+				List<String> inheritedGroups = inheritedGroupsByWorld.get(world);
+				for (String groupName : inheritedGroups) {
+					Group g = MainPlugin.getGroup(groupName);
+					if (g != null) {
+						group.addGroup(world, g);
+						System.out.println("HELLO AGAIN! " + group.getName() + " " + world + " " + groupName);
+					}
+				}
+			}
+		}
+		
+		// get users
+		ConfigurationSection usersSection = config.getConfigurationSection("users");
+		if (usersSection != null) {
+			Set<String> userKeys = usersSection.getKeys(false);
+			for (String userName : userKeys) {
+				// get user
+				ConfigurationSection userSection = usersSection.getConfigurationSection(userName);
+				User user = MainPlugin.getPlayerUser(userName);
+				if (MainPlugin.getDefaultGroup() != null && user.getGroups(null).size() == 1 && user.getGroups(null).contains(MainPlugin.getDefaultGroup())) {
+					user.removeGroup(null, MainPlugin.getDefaultGroup());
+				}
+				
+				// get group's inherited groups				
+				List<String> inheritedGroupNames = userSection.getStringList("groups");
+				if (inheritedGroupNames != null && inheritedGroupNames.size() > 0) {
+					for (String groupName : inheritedGroupNames) {
+						Group group = MainPlugin.getGroup(groupName);
+						if (group != null) {
+							user.addGroup(group);
+						}
+					}
+				}
+				
+				// get user's permissions and add to group
+				ConfigurationSection permissionsSection = userSection.getConfigurationSection("permissions");
+				if (permissionsSection != null) {
+					Set<String> permissionKeys = permissionsSection.getKeys(true);
+					for (String permission : permissionKeys) {
+						if (permissionsSection.isBoolean(permission)) {
+							boolean value = permissionsSection.getBoolean(permission, true);
+							user.addPermission((value ? "" : "-") + permission);
+						}
+					}
+				}
+				
+				// get worlds
+				if (userSection.contains("worlds")) {
+					ConfigurationSection worldsSection = userSection.getConfigurationSection("worlds");
+					Set<String> worldNames = worldsSection.getKeys(false);
+					for (String worldName : worldNames) {
+						ConfigurationSection worldSection = worldsSection.getConfigurationSection(worldName);
+												
+						// get world permissions					
+						Set<String> worldPermissions = worldSection.getKeys(true);
+						if (worldPermissions != null) {
+							for (String permission : worldPermissions) {
+								if (worldSection.isBoolean(permission)) {
+									boolean value = worldSection.getBoolean(permission, true);
+									user.addPermission(worldName, (value ? "" : "-") + permission);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		MainPlugin.yapp.saveAll();
+	}
+
 
 }
