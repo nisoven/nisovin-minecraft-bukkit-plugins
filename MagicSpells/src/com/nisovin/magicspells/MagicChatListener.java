3a9d4f3079ac4374a3b9d619af7dd6c59187134c
nisovin <nisovin@gmail.com>
2012-08-06 07:22:58 +0000

    MS: switch to async chat

--- MagicSpells/src/com/nisovin/magicspells/MagicChatListener.java
+++ MagicSpells/src/com/nisovin/magicspells/MagicChatListener.java
@@ -4,7 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class MagicChatListener implements Listener {
 
@@ -15,19 +15,19 @@ public class MagicChatListener implements Listener {
 	}
 	
 	@EventHandler(ignoreCancelled=true)
-	public void onPlayerChat(PlayerChatEvent event) {
-		final Spell spell = MagicSpells.incantations.get(event.getMessage().toLowerCase());
-		if (spell != null) {
-			final Player player = event.getPlayer();
-			Spellbook spellbook = MagicSpells.getSpellbook(player);
-			if (spellbook.hasSpell(spell)) {
-				Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
-					public void run() {
+	public void onPlayerChat(final AsyncPlayerChatEvent event) {
+		Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+			public void run() {
+				Spell spell = MagicSpells.incantations.get(event.getMessage().toLowerCase());
+				if (spell != null) {
+					Player player = event.getPlayer();
+					Spellbook spellbook = MagicSpells.getSpellbook(player);
+					if (spellbook.hasSpell(spell)) {
 						spell.cast(player);
 					}
-				});
+				}
 			}
-		}
+		});
 	}
 	
 }
