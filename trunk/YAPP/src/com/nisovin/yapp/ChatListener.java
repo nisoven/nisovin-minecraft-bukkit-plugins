9841d31d96ae6019e75725ff87b071c87d896331
nisovin@gmail.com <nisovin@gmail.com@3b5476c5-0415-9417-c352-9b123fd710cf>
2012-08-06 07:19:27 +0000

    YAPP: hopefully fix async chat stuff

--- trunk/YAPP/src/com/nisovin/yapp/ChatListener.java
+++ trunk/YAPP/src/com/nisovin/yapp/ChatListener.java
@@ -4,12 +4,12 @@ import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class ChatListener implements Listener {
 
 	@EventHandler(priority=EventPriority.LOW, ignoreCancelled=true)
-	public void onPlayerChat(PlayerChatEvent event) {
+	public void onPlayerChat(AsyncPlayerChatEvent event) {
 		User user = MainPlugin.getPlayerUser(event.getPlayer().getName());
 		String world = event.getPlayer().getWorld().getName();
 		event.setFormat(user.getPrefix(world) + "<" + user.getColor(world) + "%1$s" + ChatColor.WHITE + "> %2$s");
