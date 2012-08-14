9340e287dc0dd180420fdbd556b4e5f54d215edc
nisovin <nisovin@gmail.com>
2012-08-14 16:56:58 +0000

    YAPP: Added TagAPI support

---
+++ YAPP/src/com/nisovin/yapp/NameplateListener.java
@@ -0,0 +1,20 @@
+package com.nisovin.yapp;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.kitteh.tag.PlayerReceiveNameTagEvent;
+
+public class NameplateListener implements Listener {
+
+	@EventHandler
+	public void onNameplate(PlayerReceiveNameTagEvent event) {
+		User user = MainPlugin.getPlayerUser(event.getPlayer().getName());
+		String world = event.getPlayer().getWorld().getName();
+		ChatColor color = user.getColor(world);
+		if (color != null && color != ChatColor.WHITE) {
+			event.setTag(color + user.getName());
+		}
+	}
+	
+}
