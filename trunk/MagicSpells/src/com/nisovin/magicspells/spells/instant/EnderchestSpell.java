92ebf10b8090b8febe026b5d69d0d2646b5a7e9a
nisovin@gmail.com <nisovin@gmail.com@3b5476c5-0415-9417-c352-9b123fd710cf>
2012-11-14 00:55:23 +0000

    MS: enderchest spell can open another player's enderchest

--- trunk/MagicSpells/src/com/nisovin/magicspells/spells/instant/EnderchestSpell.java
+++ trunk/MagicSpells/src/com/nisovin/magicspells/spells/instant/EnderchestSpell.java
@@ -1,5 +1,6 @@
 package com.nisovin.magicspells.spells.instant;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import com.nisovin.magicspells.spells.InstantSpell;
@@ -14,7 +15,16 @@ public class EnderchestSpell extends InstantSpell {
 	@Override
 	public PostCastAction castSpell(Player player, SpellCastState state, float power, String[] args) {
 		if (state == SpellCastState.NORMAL) {
-			player.openInventory(player.getEnderChest());
+			if (args != null && args.length == 1 && player.hasPermission("magicspells.advanced." + internalName)) {
+				Player target = Bukkit.getPlayer(args[0]);
+				if (target == null) {
+					player.sendMessage("Invalid player target");
+					return PostCastAction.ALREADY_HANDLED;
+				}
+				player.openInventory(target.getEnderChest());
+			} else {
+				player.openInventory(player.getEnderChest());
+			}
 		}
 		return PostCastAction.HANDLE_NORMALLY;
 	}
