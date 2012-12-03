4fb7de94e8e2ca739094feb43f81efcee5b3e049
nisovin@gmail.com <nisovin@gmail.com@3b5476c5-0415-9417-c352-9b123fd710cf>
2012-12-03 17:08:04 +0000

    MS: added prefix, color-full, and color-empty options to mana ranks

--- trunk/MagicSpells/src/com/nisovin/magicspells/mana/ManaBar.java
+++ trunk/MagicSpells/src/com/nisovin/magicspells/mana/ManaBar.java
@@ -1,6 +1,7 @@
 package com.nisovin.magicspells.mana;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 import com.nisovin.magicspells.events.ManaChangeEvent;
@@ -11,6 +12,9 @@ public class ManaBar {
 	private String playerName;
 	private int maxMana;
 	private int regenAmount;
+	private String prefix;
+	private ChatColor colorFull;
+	private ChatColor colorEmpty;
 	
 	private int mana;
 	
@@ -51,6 +55,22 @@ public class ManaBar {
 	
 	public void setRegenAmount(int amount) {
 		this.regenAmount = amount;
+	}
+	
+	public void setDisplayData(String prefix, ChatColor colorFull, ChatColor colorEmpty) {
+		this.prefix = prefix;
+		this.colorFull = colorFull;
+		this.colorEmpty = colorEmpty;
+	}
+	
+	public String getPrefix() {
+		return prefix;
+	}
+	public ChatColor getColorFull() {
+		return colorFull;
+	}
+	public ChatColor getColorEmpty() {
+		return colorEmpty;
 	}
 	
 	public boolean has(int amount) {
