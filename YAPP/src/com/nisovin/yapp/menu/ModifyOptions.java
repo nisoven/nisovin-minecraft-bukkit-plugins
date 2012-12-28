b2336395732154d9317459e34ebc6a077af42020
nisovin <nisovin@gmail.com>
2012-12-28 01:58:57 +0000

    YAPP: added set group option to menu, fixed menu perm list option to track correct source

--- YAPP/src/com/nisovin/yapp/menu/ModifyOptions.java
+++ YAPP/src/com/nisovin/yapp/menu/ModifyOptions.java
@@ -27,11 +27,12 @@ public class ModifyOptions extends MenuPrompt {
 		c.sendRawMessage(Menu.TEXT_COLOR + "  1) Add a " + Menu.KEYLETTER_COLOR + "p" + Menu.KEYWORD_COLOR + "ermission " + Menu.TEXT_COLOR + "node");
 		c.sendRawMessage(Menu.TEXT_COLOR + "  2) " + Menu.KEYLETTER_COLOR + "R" + Menu.KEYWORD_COLOR + "emove " + Menu.TEXT_COLOR + "a permission node");
 		c.sendRawMessage(Menu.TEXT_COLOR + "  3) " + Menu.KEYLETTER_COLOR + "N" + Menu.KEYWORD_COLOR + "egate " + Menu.TEXT_COLOR + "a permission node");
-		c.sendRawMessage(Menu.TEXT_COLOR + "  4) Add an inherited " + Menu.KEYLETTER_COLOR + "g" + Menu.KEYWORD_COLOR + "roup");
-		c.sendRawMessage(Menu.TEXT_COLOR + "  5) Set the " + Menu.KEYWORD_COLOR + "primar" + Menu.KEYLETTER_COLOR + "y" + Menu.TEXT_COLOR + " group");
-		c.sendRawMessage(Menu.TEXT_COLOR + "  6) Remove an inherited group (" + Menu.KEYLETTER_COLOR + "u" + Menu.KEYWORD_COLOR + "ngroup" + Menu.TEXT_COLOR + ")");
-		c.sendRawMessage(Menu.TEXT_COLOR + "  7) Set the " + Menu.KEYWORD_COLOR + "chat prefi" + Menu.KEYLETTER_COLOR + "x");
-		c.sendRawMessage(Menu.TEXT_COLOR + "  8) Set the name " + Menu.KEYLETTER_COLOR + "c" + Menu.KEYWORD_COLOR + "olor");
+		c.sendRawMessage(Menu.TEXT_COLOR + "  4) " + Menu.KEYLETTER_COLOR + "S" + Menu.KEYWORD_COLOR + "et " + Menu.TEXT_COLOR + "the inherited group");
+		c.sendRawMessage(Menu.TEXT_COLOR + "  5) Add an inherited " + Menu.KEYLETTER_COLOR + "g" + Menu.KEYWORD_COLOR + "roup");
+		c.sendRawMessage(Menu.TEXT_COLOR + "  6) Set the " + Menu.KEYWORD_COLOR + "primar" + Menu.KEYLETTER_COLOR + "y" + Menu.TEXT_COLOR + " group");
+		c.sendRawMessage(Menu.TEXT_COLOR + "  7) Remove an inherited group (" + Menu.KEYLETTER_COLOR + "u" + Menu.KEYWORD_COLOR + "ngroup" + Menu.TEXT_COLOR + ")");
+		c.sendRawMessage(Menu.TEXT_COLOR + "  8) Set the " + Menu.KEYWORD_COLOR + "chat prefi" + Menu.KEYLETTER_COLOR + "x");
+		c.sendRawMessage(Menu.TEXT_COLOR + "  9) Set the name " + Menu.KEYLETTER_COLOR + "c" + Menu.KEYWORD_COLOR + "olor");
 		c.sendRawMessage(Menu.TEXT_COLOR + "  0) Show " + Menu.KEYLETTER_COLOR + "m" + Menu.KEYWORD_COLOR + "ore " + Menu.TEXT_COLOR + "options");
 		return MainPlugin.TEXT_COLOR + "Please type your selection:";
 	}
@@ -45,15 +46,17 @@ public class ModifyOptions extends MenuPrompt {
 			return Menu.REMOVE_PERMISSION;
 		} else if (input.equals("3") || input.startsWith("n")) {
 			return Menu.NEGATE_PERMISSION;
-		} else if (input.equals("4") || input.startsWith("g")) {
+		} else if (input.equals("4") || input.startsWith("s")) {
+			return Menu.SET_GROUP;
+		} else if (input.equals("5") || input.startsWith("g")) {
 			return Menu.ADD_GROUP;
-		} else if (input.equals("5") || input.startsWith("y") || input.startsWith("pri")) {
+		} else if (input.equals("6") || input.startsWith("y") || input.startsWith("pri")) {
 			return Menu.SET_PRIMARY_GROUP;
-		} else if (input.equals("6") || input.startsWith("u")) {
+		} else if (input.equals("7") || input.startsWith("u")) {
 			return Menu.REMOVE_GROUP;
-		} else if (input.equals("7") || input.startsWith("ch") || input.startsWith("pre") || input.equals("x")) {
+		} else if (input.equals("8") || input.startsWith("ch") || input.startsWith("pre") || input.equals("x")) {
 			return Menu.SET_PREFIX;
-		} else if (input.equals("8") || input.startsWith("c")) {
+		} else if (input.equals("9") || input.startsWith("c")) {
 			return Menu.SET_COLOR;
 		} else if (input.equals("0") || input.startsWith("m")) {
 			return Menu.MODIFY_OPTIONS_MORE;
