1ba730ef47a1def43f0ea685d9ffc1ef37becc1c
nisovin@gmail.com <nisovin@gmail.com@3b5476c5-0415-9417-c352-9b123fd710cf>
2012-12-29 22:04:15 +0000

    YAPP: move storage to separate classes, fix double login bug

---
+++ trunk/YAPP/src/com/nisovin/yapp/storage/MySQLStorage.java
@@ -0,0 +1,22 @@
+package com.nisovin.yapp.storage;
+
+import java.util.Map;
+
+import com.nisovin.yapp.Group;
+import com.nisovin.yapp.PermissionContainer;
+
+public class MySQLStorage implements StorageMethod {
+
+	@Override
+	public void fillGroupMap(Map<String, Group> groups) {
+	}
+
+	@Override
+	public void load(PermissionContainer container) {
+	}
+
+	@Override
+	public void save(PermissionContainer container) {
+	}
+
+}
