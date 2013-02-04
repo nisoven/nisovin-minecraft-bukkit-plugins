e4a4ae26f3acc8fbc755ecfe6bad8052003f982b
nisovin@gmail.com <nisovin@gmail.com@3b5476c5-0415-9417-c352-9b123fd710cf>
2013-02-04 18:24:45 +0000

    CL: prevent adjacent chest exploit

--- trunk/CodeLock/src/com/nisovin/codelock/ProtectListener.java
+++ trunk/CodeLock/src/com/nisovin/codelock/ProtectListener.java
@@ -3,15 +3,20 @@ package com.nisovin.codelock;
 import java.util.Iterator;
 import java.util.List;
 
+import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 
 public class ProtectListener implements Listener {
 	
 	CodeLock plugin;
+	
+	BlockFace[] directions = { BlockFace.NORTH, BlockFace.SOUTH, BlockFace.EAST, BlockFace.WEST };
 	
 	public ProtectListener(CodeLock plugin) {
 		this.plugin = plugin;
@@ -21,6 +26,20 @@ public class ProtectListener implements Listener {
 	public void onBlockBreak(BlockBreakEvent event) {
 		if (!event.getPlayer().hasPermission("codelock.bypass") && plugin.isLocked(event.getBlock())) {
 			event.setCancelled(true);
+		}
+	}
+	
+	@EventHandler(ignoreCancelled=true)
+	public void onBlockPlace(BlockPlaceEvent event) {
+		Block block = event.getBlock();
+		if (block.getType() != Material.CHEST) return;
+		
+		for (BlockFace face : directions) {
+			Block b = block.getRelative(face);
+			if (b.getType() == Material.CHEST && plugin.isLocked(b)) {
+				event.setCancelled(true);
+				return;
+			}
 		}
 	}
 	
