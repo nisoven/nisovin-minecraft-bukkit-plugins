6568572eb18f99b2fd0ffa3d23f9aca3e5c13c79
nisovin@gmail.com <nisovin@gmail.com@3b5476c5-0415-9417-c352-9b123fd710cf>
2013-04-02 23:02:01 +0000

    HP: Now uses team system

--- trunk/HealthPlates/src/com/nisovin/healthplates/HealthPlatesPlugin.java
+++ trunk/HealthPlates/src/com/nisovin/healthplates/HealthPlatesPlugin.java
@@ -1,13 +1,15 @@
 package com.nisovin.healthplates;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.minecraft.server.v1_5_R2.*;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Entity;
+import org.bukkit.craftbukkit.v1_5_R2.CraftWorld;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -15,28 +17,55 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityRegainHealthEvent;
-import org.bukkit.plugin.Plugin;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
-
 public class HealthPlatesPlugin extends JavaPlugin implements Listener {
+		
+	Scoreboard scoreboard;
 	
-	private ProtocolManager protocolManager;
+	Map<String, ScoreboardTeam> healthTeams;
+	ScoreboardTeam teamWhite;
+	ScoreboardTeam teamYellow;
+	ScoreboardTeam teamGold;
+	ScoreboardTeam teamRed;
 	
 	@Override
 	public void onEnable() {
-		protocolManager = ProtocolLibrary.getProtocolManager();
-		protocolManager.addPacketListener(new PacketChanger(this));
+		scoreboard = ((CraftWorld)Bukkit.getWorlds().get(0)).getHandle().getScoreboard();
+		teamWhite = scoreboard.createTeam("w");
+		teamWhite.setPrefix(ChatColor.WHITE.toString());
+		teamYellow = scoreboard.createTeam("y");
+		teamYellow.setPrefix(ChatColor.YELLOW.toString());
+		teamGold = scoreboard.createTeam("g");
+		teamGold.setPrefix(ChatColor.GOLD.toString());
+		teamRed = scoreboard.createTeam("r");
+		teamRed.setPrefix(ChatColor.RED.toString());
+		ScoreboardTeam[] teams = { teamWhite, teamYellow, teamGold, teamRed };
+		for (ScoreboardTeam team : teams) {
+			team.setAllowFriendlyFire(true);
+			team.setCanSeeFriendlyInvisibles(false);
+			team.setDisplayName("");
+		}
+		
+		healthTeams = new HashMap<String, ScoreboardTeam>();
+		for (Player player : Bukkit.getOnlinePlayers()) {
+			updateHealthTeam(player);
+		}
+		
 		getServer().getPluginManager().registerEvents(this, this);
+		
 	}
 	
 	@Override
 	public void onDisable() {
-		protocolManager.removePacketListeners(this);
-		HandlerList.unregisterAll((Plugin)this);
-		protocolManager = null;
+		ScoreboardTeam[] teams = { teamWhite, teamYellow, teamGold, teamRed };
+		for (ScoreboardTeam team : teams) {
+			scoreboard.removeTeam(team);
+		}
+		
+		HandlerList.unregisterAll((Listener)this);
 	}
 	
 	@Override
@@ -53,7 +82,7 @@ public class HealthPlatesPlugin extends JavaPlugin implements Listener {
 			} else if (args[0].equalsIgnoreCase("update") && args.length == 2) {
 				Player player = Bukkit.getPlayer(args[1]);
 				if (player != null) {
-					protocolManager.updateEntity(player, player.getWorld().getPlayers());
+					updateHealthTeam(player);
 				} else {
 					sender.sendMessage("No such player.");
 				}
@@ -64,6 +93,27 @@ public class HealthPlatesPlugin extends JavaPlugin implements Listener {
 		return true;
 	}
 	
+	public void updateHealthTeam(Player player) {
+		ScoreboardTeam currTeam = healthTeams.get(player.getName().toLowerCase());
+		ScoreboardTeam newTeam = getTeamByHealth(player.getHealth());
+		if (currTeam != newTeam) {
+			scoreboard.addPlayerToTeam(player.getName(), newTeam);
+			healthTeams.put(player.getName().toLowerCase(), newTeam);
+		}
+	}
+	
+	public ScoreboardTeam getTeamByHealth(int health) {
+		if (health >= 16) {
+			return teamWhite;
+		} else if (health >= 11) {
+			return teamYellow;
+		} else if (health >= 6) {
+			return teamGold;
+		} else {
+			return teamRed;
+		}
+	}
+	
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onDamage(EntityDamageEvent event) {
 		if (!(event.getEntity() instanceof Player)) return;
@@ -71,15 +121,10 @@ public class HealthPlatesPlugin extends JavaPlugin implements Listener {
 		final Player player = (Player)event.getEntity();
 		if (player.hasPermission("healthplates.nocolor")) return;
 		
-		final ChatColor currentColor = getColor(player.getHealth());
-		
 		Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
 			public void run() {
 				if (player.isValid()) {
-					ChatColor newColor = getColor(player.getHealth());
-					if (currentColor != newColor) {
-						protocolManager.updateEntity(player, getNearbyPlayers(player));
-					}
+					updateHealthTeam(player);
 				}
 			}
 		}, 1);
@@ -92,40 +137,32 @@ public class HealthPlatesPlugin extends JavaPlugin implements Listener {
 		final Player player = (Player)event.getEntity();
 		if (player.hasPermission("healthplates.nocolor")) return;
 		
-		ChatColor currentColor = getColor(player.getHealth());
-		ChatColor newColor = getColor(player.getHealth() + event.getAmount());
-		if (currentColor != newColor) {
-			Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-				public void run() {
-					protocolManager.updateEntity(player, getNearbyPlayers(player));
-				}
-			}, 1);
-		}
-	}
-	
-	public ChatColor getColor(int health) {
-		if (health > 16) {
-			return null;
-		} else if (health > 12) {
-			return ChatColor.YELLOW;
-		} else if (health > 8) {
-			return ChatColor.GOLD;
-		} else if (health > 4) {
-			return ChatColor.RED;
-		} else {
-			return ChatColor.DARK_RED;
-		}
-	}
-	
-	private List<Player> getNearbyPlayers(Player player) {
-		List<Entity> entities = player.getNearbyEntities(50, 30, 50);
-		List<Player> players = new ArrayList<Player>();
-		for (Entity e : entities) {
-			if (e instanceof Player && !((Player)e).hasPermission("healthplates.noview")) {
-				players.add((Player)e);
+		Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+			public void run() {
+				updateHealthTeam(player);
 			}
-		}
-		return players;
+		}, 1);
+	}
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		final Player player = event.getPlayer();
+		if (player.hasPermission("healthplates.nocolor")) return;
+		
+		Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+			public void run() {
+				updateHealthTeam(player);
+			}
+		}, 1);
+	}
+	
+	@EventHandler
+	public void onPlayerRespawn(PlayerRespawnEvent event) {
+		final Player player = event.getPlayer();
+		if (player.hasPermission("healthplates.nocolor")) return;
+		
+		scoreboard.addPlayerToTeam(event.getPlayer().getName(), teamWhite);
+		healthTeams.put(player.getName().toLowerCase(), teamWhite);
 	}
 	
 	public void debug(String string) {
