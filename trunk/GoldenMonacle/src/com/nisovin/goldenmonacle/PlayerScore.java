264d275e17e0a4f22ea59a5b5fce72657702afad
nisovin@gmail.com <nisovin@gmail.com@3b5476c5-0415-9417-c352-9b123fd710cf>
2013-04-11 18:05:35 +0000

    GM: golden monocle implementation

---
+++ trunk/GoldenMonacle/src/com/nisovin/goldenmonacle/PlayerScore.java
@@ -0,0 +1,39 @@
+package com.nisovin.goldenmonacle;
+
+import org.bukkit.entity.Player;
+
+public class PlayerScore implements Comparable<PlayerScore> {
+
+	String playerName;
+	int score;
+	
+	public PlayerScore(Player player, int score) {
+		this.playerName = player.getName();
+		this.score = score > 0 ? score : 0;
+	}
+	
+	public void modifyScore(int amount) {
+		int newScore = score + amount;
+		if (newScore < 0) {
+			score = 0;
+		} else {
+			score = newScore;
+		}
+	}
+	
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	@Override
+	public int compareTo(PlayerScore o) {
+		if (o.score > this.score) {
+			return 1;
+		} else if (o.score < this.score) {
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+	
+}
