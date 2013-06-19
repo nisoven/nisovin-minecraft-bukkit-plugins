ba2f087b869f1bf031ff780d3ba59ca6c1e9dd26
nisovin@gmail.com <nisovin@gmail.com@3b5476c5-0415-9417-c352-9b123fd710cf>
2013-06-19 15:33:20 +0000

    MS: added TransmuteSpell

---
+++ trunk/MagicSpells/src/com/nisovin/magicspells/spells/targeted/TransmuteSpell.java
@@ -0,0 +1,87 @@
+package com.nisovin.magicspells.spells.targeted;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
+
+import com.nisovin.magicspells.MagicSpells;
+import com.nisovin.magicspells.spells.TargetedLocationSpell;
+import com.nisovin.magicspells.util.MagicConfig;
+
+public class TransmuteSpell extends TargetedLocationSpell {
+
+	int[] blockTypes;
+	int transmuteType;
+	byte transmuteData;
+	BlockFace[] checkDirs = new BlockFace[] { BlockFace.DOWN, BlockFace.NORTH, BlockFace.SOUTH, BlockFace.EAST, BlockFace.WEST };
+	boolean checkAll = false;
+	
+	public TransmuteSpell(MagicConfig config, String spellName) {
+		super(config, spellName);
+		
+		List<Integer> list = getConfigIntList("transmutable-types", null);
+		if (list == null) list = new ArrayList<Integer>();
+		if (list.size() == 0) list.add(Material.IRON_BLOCK.getId());
+		blockTypes = new int[list.size()];
+		for (int i = 0; i < blockTypes.length; i++) {
+			blockTypes[i] = list.get(i);
+		}
+		
+		transmuteType = getConfigInt("transmute-type", Material.GOLD_BLOCK.getId());
+		transmuteData = (byte)getConfigInt("transmute-data", 0);
+	}
+
+	@Override
+	public PostCastAction castSpell(Player player, SpellCastState state, float power, String[] args) {
+		if (state == SpellCastState.NORMAL) {
+			Block block = player.getTargetBlock(MagicSpells.getTransparentBlocks(), range);
+			if (block == null) {
+				return noTarget(player);
+			}
+			
+			if (!canTransmute(block)) {
+				return noTarget(player);
+			}
+			
+			block.setTypeIdAndData(transmuteType, transmuteData, true);
+		}
+		return PostCastAction.HANDLE_NORMALLY;
+	}
+
+	@Override
+	public boolean castAtLocation(Player caster, Location target, float power) {
+		Block block = target.getBlock();
+		if (canTransmute(block)) {
+			block.setTypeIdAndData(transmuteType, transmuteData, true);
+			return true;
+		} else {
+			boolean changed = false;
+			for (BlockFace face : checkDirs) {
+				Block b = block.getRelative(face);
+				if (canTransmute(b)) {
+					b.setTypeIdAndData(transmuteType, transmuteData, true);
+					changed = true;
+					if (!checkAll) {
+						return true;
+					}
+				}
+			}
+			if (changed) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	private boolean canTransmute(Block block) {
+		return Arrays.binarySearch(blockTypes, block.getTypeId()) > 0;
+	}
+
+}
