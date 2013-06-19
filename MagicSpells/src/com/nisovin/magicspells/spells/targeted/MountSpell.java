605473874b95a41af9259acf3f732b6370d44a43
nisovin <nisovin@gmail.com>
2013-06-19 15:32:35 +0000

    MS: added 'reverse' option to mount spell

--- MagicSpells/src/com/nisovin/magicspells/spells/targeted/MountSpell.java
+++ MagicSpells/src/com/nisovin/magicspells/spells/targeted/MountSpell.java
@@ -9,38 +9,67 @@ import com.nisovin.magicspells.util.MagicConfig;
 
 public class MountSpell extends TargetedEntitySpell {
 
+	boolean reverse = false;
+	
 	public MountSpell(MagicConfig config, String spellName) {
 		super(config, spellName);
+		
+		reverse = getConfigBoolean("reverse", false);
+		
 	}
 
 	@Override
 	public PostCastAction castSpell(Player player, SpellCastState state, float power, String[] args) {
 		if (state == SpellCastState.NORMAL) {
-			if (player.getVehicle() != null) {
-				// leave stack
-				Entity veh = player.getVehicle();
-				veh.eject();
-				Entity pass = player.getPassenger();
-				if (pass != null) {
-					player.eject();
-					veh.setPassenger(pass);
+			if (!reverse) {
+				// normal - casting player mounts target
+				if (player.getVehicle() != null) {
+					// leave stack
+					Entity veh = player.getVehicle();
+					veh.eject();
+					Entity pass = player.getPassenger();
+					if (pass != null) {
+						player.eject();
+						veh.setPassenger(pass);
+					}
+				} else {
+					// join stack
+					LivingEntity target = getTargetedPlayer(player, minRange, range, true);
+					if (target != null) {
+						while (target.getPassenger() != null && target.getPassenger() instanceof LivingEntity) {
+							target = (LivingEntity)target.getPassenger();
+						}
+						player.eject();
+						target.setPassenger(player);
+						sendMessages(player, target);
+						return PostCastAction.NO_MESSAGES;
+					} else {
+						return noTarget(player);
+					}
 				}
 			} else {
-				// join stack
-				
+				// reverse - casting player forces target to mount self
 				LivingEntity target = getTargetedPlayer(player, minRange, range, true);
 				if (target != null) {
-					while (target.getPassenger() != null && target.getPassenger() instanceof LivingEntity) {
-						target = (LivingEntity)target.getPassenger();
+					// clear out any previous passengers
+					if (player.getPassenger() != null) {
+						player.eject();
 					}
-					player.eject();
-					target.setPassenger(player);
+					if (player.getVehicle() != null) {
+						player.getVehicle().eject();
+					}
+					if (target.getPassenger() != null) {
+						target.eject();
+					}
+					if (target.getVehicle() != null) {
+						target.getVehicle().eject();
+					}
+					// set passenger
+					player.setPassenger(target);
 					sendMessages(player, target);
 					return PostCastAction.NO_MESSAGES;
-				} else {
-					return noTarget(player);
 				}
-			}
+			}			
 		}
 		return PostCastAction.HANDLE_NORMALLY;
 	}
