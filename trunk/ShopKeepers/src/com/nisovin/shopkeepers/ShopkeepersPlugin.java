669a41e6f6d64f843f15b4467c5046dcc4df10de
nisovin@gmail.com <nisovin@gmail.com@3b5476c5-0415-9417-c352-9b123fd710cf>
2013-07-02 16:28:24 +0000

    SK: update for 1.6

--- trunk/ShopKeepers/src/com/nisovin/shopkeepers/ShopkeepersPlugin.java
+++ trunk/ShopKeepers/src/com/nisovin/shopkeepers/ShopkeepersPlugin.java
@@ -74,15 +74,20 @@ public class ShopkeepersPlugin extends JavaPlugin {
 		
 		// load volatile code handler
 		try {
-			Class.forName("net.minecraft.server.v1_5_R3.MinecraftServer");
-			volatileCodeHandle = new VolatileCode_1_5_R3();
-		} catch (ClassNotFoundException e_1_5_r3) {
+			Class.forName("net.minecraft.server.v1_6_R1.MinecraftServer");
+			volatileCodeHandle = new VolatileCode_1_6_R1();
+		} catch (ClassNotFoundException e_1_6_r0) {
 			try {
-				Class.forName("net.minecraft.server.MinecraftServer");
-				if (getServer().getVersion().contains("1.5")) {
-					volatileCodeHandle = new VolatileCode_1_5_Z();
+				Class.forName("net.minecraft.server.v1_5_R3.MinecraftServer");
+				volatileCodeHandle = new VolatileCode_1_5_R3();
+			} catch (ClassNotFoundException e_1_5_r3) {
+				try {
+					Class.forName("net.minecraft.server.MinecraftServer");
+					if (getServer().getVersion().contains("1.5")) {
+						volatileCodeHandle = new VolatileCode_1_5_Z();
+					}
+				} catch (ClassNotFoundException e_1_5_null) {					
 				}
-			} catch (ClassNotFoundException e_1_5_null) {					
 			}
 		}
 		if (volatileCodeHandle == null) {
