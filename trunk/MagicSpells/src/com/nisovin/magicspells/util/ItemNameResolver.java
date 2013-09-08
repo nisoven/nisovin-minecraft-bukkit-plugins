789459ab51f038a5e46752c14750d4bf3175f365
nisovin@gmail.com <nisovin@gmail.com@3b5476c5-0415-9417-c352-9b123fd710cf>
2013-09-08 07:13:17 +0000

    MS: begin update for full block/item name support, rather than id numbers

--- trunk/MagicSpells/src/com/nisovin/magicspells/util/ItemNameResolver.java
+++ trunk/MagicSpells/src/com/nisovin/magicspells/util/ItemNameResolver.java
@@ -1,12 +1,94 @@
 package com.nisovin.magicspells.util;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.MaterialData;
+
 public interface ItemNameResolver {
 
 	public ItemTypeAndData resolve(String string);
 	
+	public MagicMaterial resolve2(String string);
+	
 	public class ItemTypeAndData {
 		public int id = 0;
 		public short data = 0;
+	}
+	
+	public abstract class MagicMaterial {
+		public void setBlock(Block block) {}
+		public abstract ItemStack toItemStack();
+		public abstract ItemStack toItemStack(int quantity);
+	}
+	
+	public class MagicBlockMaterial extends MagicMaterial {
+		MaterialData data;
+		
+		public MagicBlockMaterial(MaterialData data) {
+			this.data = data;
+		}
+		
+		@Override
+		public void setBlock(Block block) {
+			BlockState state = block.getState();
+			state.setType(data.getItemType());
+			state.setData(data);
+			state.update(true);
+		}
+
+		@Override
+		public ItemStack toItemStack() {
+			return data.toItemStack();
+		}
+
+		@Override
+		public ItemStack toItemStack(int quantity) {
+			return data.toItemStack(quantity);
+		}
+	}
+	
+	public class MagicItemMaterial extends MagicMaterial {
+		Material type;
+		short data;
+
+		public MagicItemMaterial(Material type, short data) {
+			this.type = type;
+			this.data = data;
+		}
+		
+		@Override
+		public ItemStack toItemStack() {
+			return new ItemStack(type, 1, data);
+		}
+
+		@Override
+		public ItemStack toItemStack(int quantity) {
+			return new ItemStack(type, quantity, data);
+		}
+	}
+	
+	public class MagicUnknownMaterial extends MagicMaterial {
+		int type;
+		short data;
+		
+		@Override
+		public void setBlock(Block block) {
+			if (data < 16) {
+				block.setTypeIdAndData(type, (byte)data, true);
+			}
+		}		
+		
+		@Override
+		public ItemStack toItemStack() {
+			return new ItemStack(type, 1, data);
+		}
+		
+		@Override
+		public ItemStack toItemStack(int quantity) {
+			return new ItemStack(type, quantity, data);
+		}
 	}
 	
 }
--- trunk/MagicSpells/src/com/nisovin/magicspells/util/MagicItemNameResolver.java
+++ trunk/MagicSpells/src/com/nisovin/magicspells/util/MagicItemNameResolver.java
@@ -1,9 +1,27 @@
 package com.nisovin.magicspells.util;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 import org.bukkit.Material;
+import org.bukkit.TreeSpecies;
+import org.bukkit.block.BlockFace;
+import org.bukkit.material.MaterialData;
+import org.bukkit.material.Tree;
 
 public class MagicItemNameResolver implements ItemNameResolver {
 
+	Map<String, Material> nameMap = new HashMap<String, Material>();
+	Random rand = new Random();
+	
+	public MagicItemNameResolver() {
+		for (Material mat : Material.values()) {
+			nameMap.put(mat.name().toLowerCase(), mat);
+			nameMap.put(mat.name().toLowerCase().replace("_", ""), mat);
+		}
+	}
+	
 	@Override
 	public ItemTypeAndData resolve(String string) {
 		if (string == null || string.isEmpty()) return null;
@@ -32,6 +50,96 @@ public class MagicItemNameResolver implements ItemNameResolver {
 			}
 		}
 		return item;
+	}
+	
+	@Override
+	public MagicMaterial resolve2(String string) {
+		if (string == null || string.isEmpty()) return null;
+		
+		String stype;
+		String sdata;
+		if (string.contains(":")) {
+			String[] split = string.split(":", 2);
+			stype = split[0].toLowerCase();
+			sdata = split[1];
+		} else {
+			stype = string.toLowerCase();
+			sdata = "";
+		}
+		
+		Material type = nameMap.get(stype);
+		if (type == null) {
+			return resolveUnknown(stype, sdata);
+		}
+		
+		if (type.isBlock()) {
+			MaterialData data = null;
+			if (type == Material.LOG) {
+				data = getTree(sdata);
+			}
+			
+			if (data != null) {
+				return new MagicBlockMaterial(data);
+			} else if (sdata.matches("^[0-9]+$")) {
+				short durability = 0;
+				try {
+					durability = Short.parseShort(sdata);
+				} catch (NumberFormatException e) {}
+				return new MagicItemMaterial(type, durability);
+			} else {
+				return new MagicBlockMaterial(new MaterialData(type));
+			}
+		} else {
+			short durability = 0;
+			try {
+				durability = Short.parseShort(sdata);
+			} catch (NumberFormatException e) {}
+			return new MagicItemMaterial(type, durability);
+		}
+	}
+	
+	private MagicMaterial resolveUnknown(String stype, String sdata) {
+		return null;
+	}
+	
+	
+	
+	private Tree getTree(String data) {
+		TreeSpecies species = TreeSpecies.GENERIC;
+		BlockFace dir = BlockFace.UP;
+		if (data != null && data.length() > 0) {
+			String[] split = data.split(":");
+			if (split.length >= 1) {
+				if (split[0].equalsIgnoreCase("birch")) {
+					species = TreeSpecies.BIRCH;
+				} else if (split[0].equalsIgnoreCase("jungle")) {
+					species = TreeSpecies.JUNGLE;
+				} else if (split[0].equalsIgnoreCase("redwood")) {
+					species = TreeSpecies.REDWOOD;
+				} else if (split[0].equalsIgnoreCase("random")) {
+					species = TreeSpecies.values()[rand.nextInt(TreeSpecies.values().length)];
+				}
+			}
+			if (split.length >= 2) {
+				if (split[1].equalsIgnoreCase("east")) {
+					dir = BlockFace.EAST;
+				} else if (split[1].equalsIgnoreCase("west")) {
+					dir = BlockFace.WEST;
+				} else if (split[1].equalsIgnoreCase("north")) {
+					dir = BlockFace.NORTH;
+				} else if (split[1].equalsIgnoreCase("south")) {
+					dir = BlockFace.SOUTH;
+				} else if (split[1].equalsIgnoreCase("random")) {
+					int r = rand.nextInt(3);
+					if (r == 0) {
+						dir = BlockFace.EAST;
+					} else if (r == 1) {
+						dir = BlockFace.NORTH;
+					}
+				}
+			}
+		}
+		return new Tree(species, dir);
 	}
 
 }
