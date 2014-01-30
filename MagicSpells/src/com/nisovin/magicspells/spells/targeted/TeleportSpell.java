e9ba73f9ddb585c802dd125f46294c2e5b6b4188
nisovin <nisovin@gmail.com>
2014-01-30 20:40:15 +0000

    MS: added teleport spell

---
+++ MagicSpells/src/com/nisovin/magicspells/spells/targeted/TeleportSpell.java
@@ -0,0 +1,51 @@
+package com.nisovin.magicspells.spells.targeted;
+
+import org.bukkit.Location;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+
+import com.nisovin.magicspells.spelleffects.EffectPosition;
+import com.nisovin.magicspells.spells.TargetedEntitySpell;
+import com.nisovin.magicspells.spells.TargetedSpell;
+import com.nisovin.magicspells.util.MagicConfig;
+
+public class TeleportSpell extends TargetedSpell implements TargetedEntitySpell {
+
+	public TeleportSpell(MagicConfig config, String spellName) {
+		super(config, spellName);
+	}
+
+	@Override
+	public PostCastAction castSpell(Player player, SpellCastState state, float power, String[] args) {
+		if (state == SpellCastState.NORMAL) {
+			LivingEntity target = getTargetedEntity(player);
+			if (target == null) {
+				return noTarget(player);
+			}
+			boolean ok = castAtEntity(player, target, power);
+			if (!ok) {
+				return noTarget(player);
+			}
+			sendMessages(player, target);
+			return PostCastAction.NO_MESSAGES;
+		}
+		return PostCastAction.HANDLE_NORMALLY;
+	}
+
+	@Override
+	public boolean castAtEntity(Player caster, LivingEntity target, float power) {
+		Location casterLoc = caster.getLocation();
+		boolean ok = caster.teleport(target);
+		if (ok) {
+			playSpellEffects(EffectPosition.CASTER, casterLoc);
+			playSpellEffects(EffectPosition.TARGET, target.getLocation());
+		}
+		return ok;
+	}
+
+	@Override
+	public boolean castAtEntity(LivingEntity target, float power) {
+		return false;
+	}
+
+}
